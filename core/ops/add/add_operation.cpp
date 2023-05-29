b668315c953074677ea91bc68fb7b1875b37b5c7
lotus <denglian2@huawei.com>
2023-05-29 14:08:38 +0800

    t pus feat:初次提交

---
+++ core/ops/add/add_operation.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "acltransformer/ops/add_operation.h"
+#include "add_ops_runner.h"
+#include "add_ops_runner_builder.h"
+#include "add_torch_runner_builder.h"
+
+namespace AclTransformer {
+AddOperation::AddOperation(const AddParam &param) : Operation("AddOperation"), param_(param)
+{
+    runnerBuilders_ = {new AddOpsRunnerBuilder(param_), new AddTorchRunnerBuilder(param_)};
+}
+
+AddOperation::~AddOperation() {}
+
+AsdOps::Status AddOperation::InferShape(const std::vector<AsdOps::TensorDesc> &inTensorDescs,
+                                        std::vector<AsdOps::TensorDesc> &outTensorDescs)
+{
+    if (inTensorDescs.size() != 2) {
+        return AsdOps::Status::FailStatus(1, "inTensorDescs size is not 2");
+    }
+
+    outTensorDescs.resize(1);
+    outTensorDescs.at(0) = inTensorDescs.at(0);
+    return AsdOps::Status::OkStatus();
+}
+
+RunnerBuilder *AddOperation::FindBestRunnerBuilder(const VariantPack &variantPack) { return runnerBuilders_.at(1); }
+} // namespace AclTransformer
---
+++ core/ops/add/add_ops_runner.cpp
@@ -0,0 +1,41 @@
+
+
+/*
+ * Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "add_ops_runner.h"
+#include <asdops/utils/log/log.h>
+#include <asdops/params/params.h>
+
+namespace AclTransformer {
+AddOpsRunner::AddOpsRunner(AddParam &param) : OpsRunner("AddOpsRunner"), param_(param)
+{
+    ASD_LOG(INFO) << "AddOperation::AddOperation called";
+    kernelGraph_.inTensors.resize(2);
+    AsdOps::Tensor &aTensor = kernelGraph_.inTensors[0];
+    AsdOps::Tensor &bTensor = kernelGraph_.inTensors[1];
+    kernelGraph_.outTensors.resize(1);
+    AsdOps::Tensor &operationOutTensor = kernelGraph_.outTensors[0];
+
+    kernelGraph_.nodes.resize(1);
+    auto &addNode = kernelGraph_.nodes[0];
+
+    addNode.opDesc = {0, "BroadcastOperation", AsdOps::OpParam::Broadcast({AsdOps::OpParam::Broadcast::BROADCAST_ADD})};
+    addNode.inTensors = {&aTensor, &bTensor};
+    addNode.outTensors = {&operationOutTensor};
+}
+
+AddOpsRunner::~AddOpsRunner() {}
+} // namespace AclTransformer
