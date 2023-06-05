62a9ee07156971fce164beac1368c5db4932cfe3
root <18779699521>
2023-06-05 09:43:14 +0800

    add self_attention_operation, positin_embedding_operation

---
+++ core/ops/position_embedding/position_embedding_operation.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "acltransformer/ops/position_embedding_operation.h"
+#include "position_embedding_torch_runner_builder.h"
+
+namespace AclTransformer {
+PositionEmbeddingOperation::PositionEmbeddingOperation(const PositionEmbeddingParam  &param)
+    : Operation("PositionEmbeddingOperation"), param_(param)
+{
+    runnerBuilders_ = {new PositionEmbeddingTorchRunnerBuilder(param_)};
+}
+
+PositionEmbeddingOperation::~PositionEmbeddingOperation() {}
+
+AsdOps::Status PositionEmbeddingOperation::InferShape(const AsdOps::SVector<AsdOps::Tensor> &inTensors,
+                                                  AsdOps::SVector<AsdOps::TensorDesc> &outTensorDescs)
+{
+    // in : Q,[seq_len, batch, all_head_size]   position_ids,[]  cos_table,[]  sin_table[]
+    // out : Q ,[seq_len, batch, head_num, head_size]
+    if (inTensors.size() != 4) {
+        return AsdOps::Status::FailStatus(1, "inTensorDescs size is not 4 ");
+    }
+
+    outTensorDescs.resize(1);
+
+    outTensorDescs.at(0) = inTensors.at(0).desc;
+    outTensorDescs.at(0).dims.clear();
+    outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(0));
+    outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(1));
+    outTensorDescs.at(0).dims.push_back(param_.headNum);
+    outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(2) / param_.headNum);
+
+    return AsdOps::Status::OkStatus();
+}
+
+RunnerBuilder *PositionEmbeddingOperation::FindBestRunnerBuilder(const VariantPack &variantPack)
+{
+    return runnerBuilders_.at(0);
+}
+} // namespace AclTransformer
---
+++ examples/torch/operation/test_self_attention_operation_kv_cache_rand.py
@@ -0,0 +1,54 @@
+# Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+import os
+import json
+import torch
+import torch_npu
+
+
+ACLTRANSFORMER_HOME_PATH = os.environ.get("ACLTRANSFORMER_HOME_PATH")
+if ACLTRANSFORMER_HOME_PATH is None:
+    raise RuntimeError(
+        "env ACLTRANSFORMER_HOME_PATH not exist, source set_env.sh")
+
+LIB_PATH = os.path.join(ACLTRANSFORMER_HOME_PATH,
+                        "examples/libacltransformer_torch.so")
+torch.classes.load_library(LIB_PATH)
+
+
+class TestAddNormal(unittest.TestCase):
+    # [seq, batch, headNum, headSize] = [1, 1, 32, 128]
+    def test_2d(self):
+        operation = torch.classes.OperationTorch.OperationTorch()
+        operation.test()
+        # in
+        query = torch.rand(1, 1, 32, 128).npu()
+        key = torch.rand(1, 1, 32, 128).npu()
+        value = torch.rand(1, 1, 4096).npu()
+        attention_mask = torch.ones(1, 1, dtype=torch.bool).npu()
+        past_key = torch.rand(11, 1, 32, 128).npu()
+        past_value = torch.rand(11, 1, 32, 128).npu()
+
+        result, present_key, present_value = operation.execute("SelfAttentionKvCacheOperation",
+                          json.dumps({"transKey": True, "dk": 128,
+                                     "headNum": 32, "layerId": 13}),
+                          [query, key, value, attention_mask, past_key, past_value])
+        print("result:" + str(result.shape))
+        print("present_key:" + str(present_key.shape))
+        print("present_value:" + str(present_value.shape))
+
+
+if __name__ == '__main__':
+    unittest.main()
