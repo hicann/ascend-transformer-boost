6c78fd7562b68f280b9ae3588e9ae75dc03e08c0
ascend <root@ubuntu.com>
2023-06-07 23:30:18 +0800

    add norm_operation, add chatGlmBlock

---
+++ core/ops/norm/norm_operation.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "acltransformer/ops/norm_operation.h"
+#include "acltransformer/config.h"
+#include "norm_torch_runner_builder.h"
+
+namespace AclTransformer {
+NormOperation::NormOperation(const NormParam &param) : Operation("NormOperation"), param_(param)
+{
+    runnerBuilders_ = {new NormTorchRunnerBuilder(param_)};
+}
+
+NormOperation::~NormOperation() {}
+
+AsdOps::Status NormOperation::InferShape(const AsdOps::SVector<AsdOps::Tensor> &inTensors,
+                                            AsdOps::SVector<AsdOps::TensorDesc> &outTensorDescs)
+{
+    if (inTensors.size() != 3) {
+        return AsdOps::Status::FailStatus(1, "inTensorDescs size is not 3");
+    }
+
+    outTensorDescs.resize(1);
+    outTensorDescs.at(0) = inTensors.at(0).desc;
+    return AsdOps::Status::OkStatus();
+}
+
+RunnerBuilder *NormOperation::FindBestRunnerBuilder(const VariantPack &variantPack)
+{
+    return runnerBuilders_.at(0);
+}
+} // namespace AclTransformer
