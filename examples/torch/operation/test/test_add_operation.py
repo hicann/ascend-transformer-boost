143741873a3c2ab81e6a7cfa10980144f9a4c2e8
ascend <root@ubuntu.com>
2023-06-09 20:31:01 +0800

    feat: add OperationTest.py

---
+++ examples/torch/operation/test/test_add_operation.py
@@ -0,0 +1,50 @@
+# Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import os
+import unittest
+import torch
+import torch_npu
+
+
+sys.path.append(os.path.dirname(__file__))
+import operation_test  # NOQA: E402
+
+
+OP_NAME = "AddOperation"
+
+
+class TestAddOperation(operation_test.OperationTest):
+    def golden_calc(self, in_tensors):
+        return [in_tensors[0] + in_tensors[1]]
+
+    def test_2d_float(self):
+        self.execute(OP_NAME, {"scale": 1}, [torch.randn(
+            1024, 1024).npu(), torch.randn(1024, 1024).npu()])
+
+    def test_2d_half(self):
+        self.execute(OP_NAME, {"scale": 1}, [torch.randn(
+            1024, 1024).npu().half(), torch.randn(1024, 1024).npu().half()])
+
+    def test_2d_float_scale(self):
+        self.execute(OP_NAME, {"scale": 2}, [torch.randn(
+            1024, 1024).npu(), torch.randn(1024, 1024).npu()])
+
+    def test_2d_half_scale(self):
+        self.execute(OP_NAME, {"scale": 2}, [torch.randn(
+            1024, 1024).npu().half(), torch.randn(1024, 1024).npu().half()])
+
+
+if __name__ == '__main__':
+    unittest.main()
