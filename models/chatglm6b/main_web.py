d92990c9c118f2eefc3c41a75bec9c196ddc03cc
wen-jie666 <wenjie39@huawei.com>
2023-06-25 10:37:32 +0800

    chatglm6b WebUI

---
+++ models/chatglm6b/main_web.py
@@ -0,0 +1,151 @@
+import transformers
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+# 适配昇腾NPU
+import torch_npu
+from torch_npu.contrib import transfer_to_npu
+torch.npu.set_device(torch.device("npu:0"))
+
+import gradio as gr
+import mdtex2html
+
+# 使用二进制优化，消除动态shape的编译问题
+torch.npu.set_compile_mode(jit_compile=False)
+option = {}
+option["NPU_FUZZY_COMPILE_BLACKLIST"] = "Tril"
+torch.npu.set_option(option)
+
+tokenizer = AutoTokenizer.from_pretrained("./", trust_remote_code=True)
+model = AutoModel.from_pretrained("./", trust_remote_code=True).half().npu()
+
+"""
+Transformer initialization
+"""
+
+# 修改transformers的TopPLogitsWarper
+def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+    sorted_logits, sorted_indices = torch.sort(scores, descending=False)
+    # cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
+    cumulative_probs = sorted_logits.softmax(
+        dim=-1).cpu().float().cumsum(dim=-1).to(sorted_logits.device).to(sorted_logits.dtype)
+
+    # Remove tokens with cumulative top_p above the threshold (token with 0 are kept)
+    sorted_indices_to_remove = cumulative_probs <= (1 - self.top_p)
+    if self.min_tokens_to_keep > 1:
+        # Keep at least min_tokens_to_keep
+        sorted_indices_to_remove[..., -self.min_tokens_to_keep:] = 0
+
+    # scatter sorted tensors to original indexing
+    indices_to_remove = sorted_indices_to_remove.scatter(
+        1, sorted_indices, sorted_indices_to_remove)
+    scores = scores.masked_fill(indices_to_remove, self.filter_value)
+    return scores
+
+transformers.generation.TopPLogitsWarper.__call__ = __call__
+
+# 优化ND NZ排布，消除transdata
+soc_version = torch_npu._C._npu_get_soc_version()
+if soc_version in [104, 220, 221, 222, 223]:
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.Linear):
+            module.weight.data = module.weight.data.npu_format_cast(2)
+    print("soc_version:", soc_version, " is 910B, support ND")
+else:
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.Linear):
+            module.weight.data = module.weight.data.npu_format_cast(29)
+    print("soc_version:", soc_version, " is not 910B, support NZ")
+
+for name, module in model.named_modules():
+    if isinstance(module, torch.nn.Embedding):
+        module.weight.data = module.weight.data.npu_format_cast(2)
+
+"""
+WebUI initialization
+"""
+
+def postprocess(self, y):
+    if y is None:
+        return []
+    for i, (message, response) in enumerate(y):
+        y[i] = (
+            None if message is None else mdtex2html.convert((message)),
+            None if response is None else mdtex2html.convert(response),
+        )
+    return y
+
+gr.Chatbot.postprocess = postprocess
+
+def parse_text(text):
+    lines = text.split("\n")
+    lines = [line for line in lines if line != ""]
+    count = 0
+    for i, line in enumerate(lines):
+        if "```" in line:
+            count += 1
+            items = line.split('`')
+            if count % 2 == 1:
+                lines[i] = f'<pre><code class="language-{items[-1]}">'
+            else:
+                lines[i] = f'<br></code></pre>'
+        else:
+            if i > 0:
+                if count % 2 == 1:
+                    line = line.replace("`", "\`")
+                    line = line.replace("<", "&lt;")
+                    line = line.replace(">", "&gt;")
+                    line = line.replace(" ", "&nbsp;")
+                    line = line.replace("*", "&ast;")
+                    line = line.replace("_", "&lowbar;")
+                    line = line.replace("-", "&#45;")
+                    line = line.replace(".", "&#46;")
+                    line = line.replace("!", "&#33;")
+                    line = line.replace("(", "&#40;")
+                    line = line.replace(")", "&#41;")
+                    line = line.replace("$", "&#36;")
+                lines[i] = "<br>"+line
+    text = "".join(lines)
+    return text
+
+
+def predict(input, chatbot, max_length, top_p, temperature, history):
+    chatbot.append((parse_text(input), ""))
+    for response, history in model.stream_chat(tokenizer, input, history, max_length=max_length, top_p=top_p,
+                                               temperature=temperature):
+        chatbot[-1] = (parse_text(input), parse_text(response))       
+        yield chatbot, history
+
+
+def reset_user_input():
+    return gr.update(value='')
+
+
+def reset_state():
+    return [], []
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10)
+            with gr.Column(min_width=32, scale=1):
+                submitBtn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+
+
+    history = gr.State([])
+
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history], [chatbot, history],
+                    show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+
+demo.queue().launch(share=False, inbrowser=True)
