fcfedd4d461bc238e57e0a67c430bb8413683f57
吴铭泾 <wumingjing@huawei.com>
2023-07-11 03:55:54 +0000

    update models/chatglm6b/main_multi_performance_test.py.
    Signed-off-by: 吴铭泾 <wumingjing@huawei.com>

--- models/chatglm6b/main_multi_performance_test.py
+++ models/chatglm6b/main_multi_performance_test.py
@@ -2,7 +2,6 @@ from rand_tensor_pverformance import full_and_incremental_test, load_model
 import torch_npu
 
 if __name__ == "__main__":
-    # torch.npu.set_device(torch.device("npu:3"))
     file = open("multi_performance_test.csv", 'w')
     file.write(f"Batch,max_seq_token,input_seq_len(Encoding),output_seq_len(Decoding),TokensPerSecond(ms),ResponseTime(ms),FirstTokenTime(ms),TimePerTokens(ms)\n")
     model = load_model()
