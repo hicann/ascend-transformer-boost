4efa27e28b31065a3366b2b7fecd24c91215e9ec
wu_mingjing <wumingjing@huawei.com>
2023-07-18 15:17:22 +0800

    llama7b layer transform to operation

---
+++ examples/ops/llama7b/llama7blayer_operation.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "llama7blayer_operation.h"
+#include "acltransformer/ops/add_operation.h"
+#include "acltransformer/ops/norm_operation.h"
+#include "acltransformer/ops/linear_operation.h"
+#include "acltransformer/ops/position_embedding_1d_split_operation.h"
+#include "acltransformer/ops/self_attention_kv_cache_operation.h"
+#include "acltransformer/ops/self_attention_operation.h"
+#include "acltransformer/ops/mlp_operation.h"
+#include "acltransformer/ops/rms_norm_operation.h"
+#include "acltransformer/ops/transpose_operation.h"
+
+namespace AclTransformer {
+enum LLaMA7BLayerTensorId {
+    IN_HIDDENSTATES = 0,
+    IN_NORMWEIGHT,
+    IN_QMIXDWEIGHT,
+    IN_QMIXDBIAS,
+    IN_KMIXDWEIGHT,
+    IN_KMIXDBIAS,
+    IN_VMIXDWEIGHT,
+    IN_VMIXDBIAS,
+    IN_SELFOUTLINEARWEIGHT,
+    IN_SELFOUTLINEARBIAS,
+    IN_SELFOUTNORMWEIGHT,
+    IN_MLPGATEWEIGHT,
+    IN_MLPDOWNWEIGHT,
+    IN_MLPUPWEIGHT,
+    IN_POSITIONIDS,
+    IN_COSTABLE,
+    IN_SINTABLE,
+    IN_ATTENTIONMASK,
+    IN_PASTKEY,
+    IN_PASTVALUE,
+    OUT_LLAMA7BLAYEROUT,
+    OUT_PRESENTKEY,
+    OUT_PRESENTVALUE,
+    INTERMIDATE_INPUTNORMOUT,
+    INTERMIDATE_MIXEDQ,
+    INTERMIDATE_MIXEDK,
+    INTERMIDATE_MIXEDV,
+    INTERMIDATE_POSITIONEMBEDQ,
+    INTERMIDATE_POSITIONEMBEDK,
+    INTERMIDATE_TRANSPOSEVOUT,
+    INTERMIDATE_SELFOUT,
+    INTERMIDATE_SELFLINEAROUT,
+    INTERMIDATE_SELFRESIDUALADDOUT,
+    INTERMIDATE_SELFNORMOUT,
+    INTERMIDATE_MLPOUT,
+
+};
+
+static const uint64_t IN_TENSOR_COUNT = 20;
+static const uint64_t OUT_TENSOR_COUNT = 3;
+static const uint64_t INTERMEDIATE_TENSOR_COUNT = 12;
+static const uint64_t NODE_COUNT = 13;
+
+LLaMA7BLayerOperation::LLaMA7BLayerOperation(const LLaMA7BLayerParam &param)
+    : GraphOperation("LLaMA7BLayerOperation"), param_(param)
+{
+    opGraph_.inTensorSize = IN_TENSOR_COUNT;
+    opGraph_.outTensorSize = OUT_TENSOR_COUNT;
+    opGraph_.intermediateTensorSize = INTERMEDIATE_TENSOR_COUNT;
+    opGraph_.nodes.resize(NODE_COUNT);
+
+    size_t nodeId = 0;
+    GraphOperation::Node &inputNormNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &mixdQLinearNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &mixdKLinearNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &mixdVLinearNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &qPositionEmbeddingNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &kPositionEmbeddingNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &vTransposeNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &selfAttentionKvCacheNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &selfOutLinearNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &selfResidualAddNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &selfNormNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &mlpNode = opGraph_.nodes.at(nodeId++);
+    GraphOperation::Node &mlpResidualAddNode = opGraph_.nodes.at(nodeId++);
+
+    // TODO 参数是对的吗？
+    inputNormNode.operation.reset(new AclTransformer::RmsNormOperation({param_.rmsNormEps}));
+    inputNormNode.inTensorIds = {IN_HIDDENSTATES, IN_NORMWEIGHT};
+    inputNormNode.outTensorIds = {INTERMIDATE_INPUTNORMOUT};
+
+    mixdQLinearNode.operation.reset(new AclTransformer::LinearOperation({}));
+    mixdQLinearNode.inTensorIds = {INTERMIDATE_INPUTNORMOUT, IN_QMIXDWEIGHT, IN_QMIXDBIAS};
+    mixdQLinearNode.outTensorIds = {INTERMIDATE_MIXEDQ};
+
+    mixdKLinearNode.operation.reset(new AclTransformer::LinearOperation({}));
+    mixdKLinearNode.inTensorIds = {INTERMIDATE_INPUTNORMOUT, IN_KMIXDWEIGHT, IN_KMIXDBIAS};
+    mixdKLinearNode.outTensorIds = {INTERMIDATE_MIXEDK};
+    
+    mixdVLinearNode.operation.reset(new AclTransformer::LinearOperation({}));
+    mixdVLinearNode.inTensorIds = {INTERMIDATE_INPUTNORMOUT, IN_VMIXDWEIGHT, IN_VMIXDBIAS};
+    mixdVLinearNode.outTensorIds = {INTERMIDATE_MIXEDV};
+
+    // TODO 参数是对的吗？
+    qPositionEmbeddingNode.operation.reset(new AclTransformer::PositionEmbedding1dSplitOperation({param_.headNum}));
+    qPositionEmbeddingNode.inTensorIds = {INTERMIDATE_MIXEDQ, IN_POSITIONIDS, IN_COSTABLE, IN_SINTABLE};
+    qPositionEmbeddingNode.outTensorIds = {INTERMIDATE_POSITIONEMBEDQ};
+
+    kPositionEmbeddingNode.operation.reset(new AclTransformer::PositionEmbedding1dSplitOperation({param_.headNum}));
+    kPositionEmbeddingNode.inTensorIds = {INTERMIDATE_MIXEDK, IN_POSITIONIDS, IN_COSTABLE, IN_SINTABLE};
+    kPositionEmbeddingNode.outTensorIds = {INTERMIDATE_POSITIONEMBEDk};
+
+    // TODO 参数是对的吗？
+    vTransposeNode.operation.reset(new AclTransformer::TransposeOperation({0, 1}));
+    vTransposeNode.inTensorIds = {INTERMIDATE_MIXEDV};
+    vTransposeNode.outTensorIds = {INTERMIDATE_TRANSPOSEVOUT};
+
+    // TODO 参数是对的吗？
+    selfAttentionKvCacheNode.operation.reset(new AclTransformer::SelfAttentionKvCacheOperation(
+        {param_.dk, param_.headNum, param_.model}));
+    selfAttentionKvCacheNode.inTensorIds = {INTERMIDATE_POSITIONEMBEDQ,
+                                            INTERMIDATE_POSITIONEMBEDK,
+                                            INTERMIDATE_TRANSPOSEVOUT,
+                                            IN_ATTENTIONMASK,
+                                            IN_PASTKEY,
+                                            IN_PASTVALUE};
+    selfAttentionKvCacheNode.outTensorIds = {INTERMIDATE_SELFOUT, OUT_PRESENTKEY, OUT_PRESENTVALUE};
+
+    // TODO viewfunc 怎么替换到这
+    // selfAttentionKvCacheNode.inTensorViewFuncs.resize(selfAttentionKvCacheNode.inTensorIds.size());
+    // selfAttentionKvCacheNode.inTensorViewFuncs.at(2) = [=](const AsdOps::SVector<int64_t> &oldDims,
+    //                                                        AsdOps::SVector<int64_t> &newDims) {
+    //     newDims = {oldDims.at(0), oldDims.at(1), qPositionEmbeddingParam.headNum,
+    //                oldDims.at(2) / qPositionEmbeddingParam.headNum};
+    // };
+
+    selfOutLinearNode.operation.reset(new AclTransformer::LinearOperation({}));
+    selfOutLinearNode.inTensorIds = {INTERMIDATE_SELFOUT, IN_SELFOUTLINEARWEIGHT, IN_SELFOUTLINEARBIAS};
+    selfOutLinearNode.outTensorIds = {INTERMIDATE_SELFLINEAROUT};
+
+    selfResidualAddNode.operation.reset(new AclTransformer::AddOperation({}));
+    selfResidualAddNode.inTensorIds = {IN_HIDDENSTATES, INTERMIDATE_SELFLINEAROUT};
+    selfResidualAddNode.outTensorIds = {INTERMIDATE_SELFRESIDUALADDOUT};
+
+    selfNormNode.operation.reset(new AclTransformer::NormOperation({param_.rmsNormEps}));
+    selfNormNode.inTensorIds = {INTERMIDATE_SELFRESIDUALADDOUT, IN_SELFOUTNORMWEIGHT};
+    selfNormNode.outTensorIds = {INTERMIDATE_SELFNORMOUT};
+
+    mlpNode.operation.reset(new AclTransformer::MlpOperation({}));
+    mlpNode.inTensorIds = {INTERMIDATE_SELFNORMOUT, IN_MLPGATEWEIGHT, IN_MLPDOWNWEIGHT, IN_MLPUPWEIGHT};
+    mlpNode.outTensorIds = {INTERMIDATE_MLPOUT};
+
+    mlpResidualAddNode.operation.reset(new AclTransformer::AddOperation({}));
+    mlpResidualAddNode.inTensorIds = {INTERMIDATE_SELFRESIDUALADDOUT, INTERMIDATE_MLPOUT};
+    mlpResidualAddNode.outTensorIds = {OUT_LLAMA7BLAYEROUT};
+}
+
+LLaMA7BLayerOperation::~LLaMA7BLayerOperation() {}
+
+uint64_t LLaMA7BLayerOperation::GetInTensorCount() const { return IN_TENSOR_COUNT; }
+
+uint64_t LLaMA7BLayerOperation::GetOutTensorCount() const { return OUT_TENSOR_COUNT; }
+
+AsdOps::Status LLaMA7BLayerOperation::InferShapeImpl(const AsdOps::SVector<AsdOps::Tensor> &inTensors,
+                                                       AsdOps::SVector<AsdOps::TensorDesc> &outTensorDescs) const
+{
+    const AsdOps::Tensor &keyTensor = inTensors.at(IN_PASTKEY);
+    const AsdOps::Tensor &valueTensor = inTensors.at(IN_PASTVALUE);
+    outTensorDescs.at(0) = inTensors.at(0).desc;
+    outTensorDescs.at(1) = keyTensor.desc;
+    outTensorDescs.at(1).dims.at(0) += 1;
+    outTensorDescs.at(2) = ValueTensor.desc;
+    outTensorDescs.at(2).dims.at(0) += 1;
+    return AsdOps::Status::OkStatus();
+}
+} // namespace AclTransformer
