b2f1bcfee861e10be41ff8c75869d51b2e137f4e
lord_of_ironhill <suiweiyi@huawei.com>
2023-07-26 21:12:22 +0800

    self attention kv cache 适配910 a

--- core/ops/self_attention_kv_cache/self_attention_kv_cache_ops_runner_builder.h
+++ core/ops/self_attention_kv_cache/self_attention_kv_cache_ops_runner_builder.h
@@ -20,6 +20,7 @@
 #include "acltransformer/params/self_attention_kv_cache.h"
 #include "self_attention_kv_cache_ops_chatglm6b_runner.h"
 #include "self_attention_kv_cache_ops_llama7b_runner.h"
+#include "self_attention_kv_cache_ops_chatglm6b_runner_910a.h"
 
 namespace AclTransformer {
 class SelfAttentionKvCacheOpsRunnerBuilder : public RunnerBuilder {
@@ -29,7 +30,11 @@ public:
     Runner *Build() override
     {
         if (param_.model == "chatglm6b") {
-            return new SelfAttentionKvCacheOpsChatGlm6bRunner(param_);
+            if (AsdOps::GetSingleton<config>().Is910B()) {
+                return new SelfAttentionKvCacheOpsChatGlm6bRunner(param_);
+            } else {
+                return new SelfAttentionKvCacheOpsChatGlm6bRunner910a(param_);
+            }
         } else if (param_.model == "llama7b") {
             return new SelfAttentionKvCacheOpsLlama7bRunner(param_);
         } else {
