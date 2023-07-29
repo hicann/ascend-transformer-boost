c156053999283ae29e58de2d4174d113971c5a9c
吴铭泾 <wumingjing@huawei.com>
2023-07-29 06:47:51 +0000

    !303 add modeling llama layer performance * add modeling_llama_layer_performance

--- examples/llama/transformers_patch/operation/modeling_llama_rms_norm.sh
+++ examples/llama/transformers_patch/operation/modeling_llama_rms_norm.sh
@@ -1,1 +1,2 @@
-cp modeling_llama_rms_norm.py /home/ma-user/anaconda3/envs/MindSpore/lib/python3.7/site-packages/transformers/models/llama/modeling_llama.py
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cp $SCRIPT_DIR/modeling_llama_rms_norm.py /home/ma-user/anaconda3/envs/MindSpore/lib/python3.7/site-packages/transformers/models/llama/modeling_llama.py
