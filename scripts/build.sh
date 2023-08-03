e7dfdad55a2b5f674a6654412a01c7d559de0e9c
Lord_of_Ironhill <suiweiyi@huawei.com>
2023-08-03 01:25:21 +0000

    !321 fix: fix consine similarity & build.sh bug * fix cosine similarity bug

--- scripts/build.sh
+++ scripts/build.sh
@@ -167,6 +167,10 @@ function fn_init_pytorch_env()
 
 function fn_copy_tools()
 {
+    if [ -d "$OUTPUT_DIR/acltransformer/tools" ];then
+        rm -rf $OUTPUT_DIR/acltransformer/tools
+    fi
+    mkdir $OUTPUT_DIR/acltransformer/tools
     cp -r $CODE_ROOT/tools/python_tools $OUTPUT_DIR/acltransformer/tools/python_tools
 }
 
