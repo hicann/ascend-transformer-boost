afc4cf58df80f44a7bc86efe4920c8d9e7490bb1
纪涛 <jitao12@huawei.com>
2023-08-07 14:08:40 +0000

    !359 Modify dockerfile of chatglm6b 910b * Modify Dockerfile for Chatglm6b 910B

--- examples/chatglm6b/dockerfile/build_docker.sh
+++ examples/chatglm6b/dockerfile/build_docker.sh
@@ -3,4 +3,4 @@ if [ $# -ne 1 ]; then
 	echo "usage: $0 [arg], arg in the format of http://..."
 	exit 1
 fi
-docker build --no-cache --network=host --build-arg http_proxy=$1 --build-arg https_proxy=$http_proxy -t acltransformer_chatglm6b:v1 .
+docker build --no-cache --network=host --build-arg http_proxy=$1 --build-arg https_proxy=$http_proxy -t acltransformer_chatglm6b:v1 -f Dockerfile_dev .
