c68d3b6c342cfa8c428acd156deb1934d7ad010c
纪涛 <jitao12@huawei.com>
2023-08-07 12:59:08 +0000

    !355 Add dockerfile of chatglm6b 910b * Add dockerfile of chatglm6b 910b

---
+++ examples/chatglm6b/dockerfile/build_docker.sh
@@ -0,0 +1,6 @@
+if [ $# -ne 1 ]; then
+	echo "error: need one argument describing your proxy info."
+	echo "usage: $0 [arg], arg in the format of http://..."
+	exit 1
+fi
+docker build --no-cache --network=host --build-arg http_proxy=$1 --build-arg https_proxy=$http_proxy -t acltransformer_chatglm6b:v1 .
