31c41f73601bf8e264a19ffa423457fea2405ab6
qinsichun <qinsichun@huawei.com>
2023-08-11 14:43:12 +0800

    test_pos

---
+++ tests/unittest/ops/position_embedding_1d_split_fusion/posion_embedding_1d_split_fusion_test.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <torch/torch.h>
+#include <asdops/utils/log/log.h>
+#include "tests/unittest/test_util/test_common.h"
+#include "acltransformer/ops/position_embedding_1d_fusion_operation.h"
+#include "tests/unittest/test_util/op_test.h"
+#include <ATen/ATen.h>
+#include "acltransformer/torch/torch_util.h"
+#include "acltransformer/handle.h"
+using namespace AclTransformer;
+using namespace AsdOps;
+constexpr float ATOL = 0.0001;
+constexpr float RTOL = 0.0001;
+TEST(TestPositionEmbedding1dSplitFusionOperation, InferShape){
+
+}
