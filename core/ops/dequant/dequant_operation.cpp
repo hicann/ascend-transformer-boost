739a3d5f03b978523049393323b9ca27397c1919
lin_jing <linjing28@huawei.com>
2023-08-15 16:33:33 +0800

    添加dequant接入整网

---
+++ core/ops/dequant/dequant_operation.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "acltransformer/ops/dequant_operation.h"
+#include <asdops/utils/singleton/singleton.h>
+#include "acltransformer/config.h"
+#include "dequant_ops_runner_builder.h"
+
+namespace AclTransformer {
+DequantOperation::DequantOperation(const DequantParam &param) : Operation("DequantOperation"), param_(param)
+{
+    runnerBuilders_ = {new DequantOpsRunnerBuilder(param_)};
+}
+
+DequantOperation::~DequantOperation() {}
+
+uint64_t DequantOperation::GetInTensorCount() const { return 2; }
+
+uint64_t DequantOperation::GetOutTensorCount() const { return 1; }
+
+AsdOps::Status DequantOperation::InferShapeImpl(const AsdOps::SVector<AsdOps::Tensor> &inTensors,
+                                              AsdOps::SVector<AsdOps::TensorDesc> &outTensorDescs) const
+{
+    outTensorDescs.at(0) = inTensors.at(0).desc;
+    outTensorDescs.at(0).dtype = AsdOps::TENSOR_DTYPE_FLOAT16;
+    outTensorDescs.at(0).format = AsdOps::TENSOR_FORMAT_ND;
+    return AsdOps::Status::OkStatus();
+}
+
+RunnerBuilder *DequantOperation::FindBestRunnerBuilder() const
+{
+    size_t index = 0;
+    return runnerBuilders_.at(index);
+}
+} // namespace AclTransformer
