dd01328b486a10cffda1c5a2e2a2e9d714837b24
qinsichun <qinsichun@huawei.com>
2023-08-22 17:40:14 +0800

    add_test_position1dsplit

---
+++ tests/pythontest/operations/position_embedding_1d_split/test_position_embedding_1d_split_operation.py
@@ -0,0 +1,51 @@
+# Copyright(C) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import os
+import unittest
+import torch
+import torch_npu
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+import operation_test  # NOQA: E402
+
+
+OP_NAME = "PositionEmbedding1dSplitOperation"
+PARAM = {"headNum": 32}
+INTENSOR0 = os.path.join(os.getenv("ACLTRANSFORMER_TESTDATA"),
+                         "tensors/operations/position_embedding_1d_split", "inTensor0.bin")
+INTENSOR1 = os.path.join(os.getenv("ACLTRANSFORMER_TESTDATA"),
+                         "tensors/operations/position_embedding_1d_split", "inTensor1.bin")
+INTENSOR2 = os.path.join(os.getenv("ACLTRANSFORMER_TESTDATA"),
+                         "tensors/operations/position_embedding_1d_split", "inTensor2.bin")
+INTENSOR3 = os.path.join(os.getenv("ACLTRANSFORMER_TESTDATA"),
+                         "tensors/operations/position_embedding_1d_split", "inTensor3.bin")
+OUTTENSOR0 = os.path.join(os.getenv(
+    "ACLTRANSFORMER_TESTDATA"), "tensors/operations/position_embedding_1d_split", "outTensor0.bin")
+
+
+class TestPositionEmbeddingOperation(operation_test.OperationTest):
+    def golden_calc(self, in_tensors):
+        return [self.get_tensor(OUTTENSOR0).npu()]
+
+    def test(self):
+        self.execute(OP_NAME, PARAM, [self.get_tensor(INTENSOR0).npu(),
+                                      self.get_tensor(INTENSOR1).npu(),
+                                      self.get_tensor(INTENSOR2).npu(),
+                                      self.get_tensor(INTENSOR3).npu()])
+
+
+if __name__ == '__main__':
+    unittest.main()
