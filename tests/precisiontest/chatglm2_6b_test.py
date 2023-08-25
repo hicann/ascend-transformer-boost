0bd3f094113474c26f9eea96fb450c3ff48f94e0
wumingjing <wumingjing@huawei.com>
2023-08-25 11:17:25 +0800

    add precision test

---
+++ tests/precisiontest/chatglm2_6b_test.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../..')
+import tests.dailytest.model_test as model_test
+import os
+
+CHATGLM2_6B_PATH = os.path.join(model_test.ACLTRANSFORMER_HOME_PATH, "examples/chatglm2_6b")
+RUN_SHELL_PATH = os.path.join(CHATGLM2_6B_PATH, "run.sh")
+MODEL_SCRIPT_PATH = os.path.join(CHATGLM2_6B_PATH, "patches/models/modeling_chatglm_model_flashattention_temp_performance.py")
+# GOLDEN_SCRIPT_PATH = os.path.join(CHATGLM2_6B_PATH, "patches/models/modeling_chatglm_model_precision.py")
+
+class Chatglm2_6BModelTest(model_test.ModelTest):
+    def __init__(self, model_name) -> None:
+        super().__init__(model_name)
+    
+    def generate_precision_performance(self):
+        os.system(f"bash {RUN_SHELL_PATH} {MODEL_SCRIPT_PATH} --precision")
+        # os.system(f"bash {RUN_SHELL_PATH} {GOLDEN_SCRIPT_PATH} --precision")
+        self.append_precision_golden(CHATGLM2_6B_PATH + "/hidden_states_golden.pth")
+        self.append_precision_result(CHATGLM2_6B_PATH + "/hidden_states.pth")
+        self.precision_compare()
+
+def main():
+    test_body = Chatglm2_6BModelTest("chatglm2_6b")
+    test_body.generate_precision_performance()
+
+main()
