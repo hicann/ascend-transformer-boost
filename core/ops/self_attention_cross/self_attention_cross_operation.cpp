c25e10539406e9eee6fb84eeec15e1c74cc01c06
liuwhite97 <liuzaisheng@huawei.com>
2023-09-01 15:54:38 +0800

    add llama_adapter self_attention with no adapter

---
+++ core/ops/self_attention_cross/self_attention_cross_operation.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "acltransformer/ops/self_attention_cross_operation.h"
+#include <asdops/utils/singleton/singleton.h>
+#include "acltransformer/config.h"
+#include "self_attention_cross_ops_runner_builder.h"
+
+namespace AclTransformer {
+SelfAttentionCrossOperation::SelfAttentionCrossOperation(const SelfAttentionCrossParam &param)
+    : Operation("SelfAttentionCrossParam"), param_(param)
+{
+#ifdef USE_TORCH_RUNNER
+    runnerBuilders_ = {new SelfAttentionCrossOpsRunnerBuilder(param_)};
+#else
+    runnerBuilders_ = {new SelfAttentionCrossOpsRunnerBuilder(param_)};
+#endif
+}
+
+SelfAttentionCrossOperation::~SelfAttentionCrossOperation() {}
+
+uint64_t SelfAttentionCrossOperation::GetInTensorCount() const {
+    if (param_.model == "llama_adapter") {
+        return 6;
+    } else if (param_.model == "llama_adapter_a") {
+        return 9;
+    } else if (param_.model == "llama_adapter_encoder") {
+        return 4;
+    } else if (param_.model == "llama_adapter_encoder_a") {
+        return 7;
+    } else {
+        return 6;
+    }
+}
+
+uint64_t SelfAttentionCrossOperation::GetOutTensorCount() const { return 3; }
+
+AsdOps::Status SelfAttentionCrossOperation::InferShapeImpl(const AsdOps::SVector<AsdOps::Tensor> &inTensors,
+                                                             AsdOps::SVector<AsdOps::TensorDesc> &outTensorDescs) const
+{
+    if (param_.model == "llama_adapter" || param_.model == "llama_adapter_a") {
+        outTensorDescs.at(0) = inTensors.at(0).desc;
+        outTensorDescs.at(0).dims.clear();
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(0));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(1));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(2) * inTensors.at(0).desc.dims.at(3));
+        outTensorDescs.at(1) = inTensors.at(3).desc;
+        outTensorDescs.at(1).dims.at(1) = outTensorDescs.at(1).dims.at(1) + 1;
+        outTensorDescs.at(2) = inTensors.at(4).desc;
+        outTensorDescs.at(2).dims.at(1) = outTensorDescs.at(2).dims.at(1) + 1;
+
+    } else if (param_.model == "llama_adapter_encoder" || param_.model == "llama_adapter_encoder_a") {
+        outTensorDescs.at(0) = inTensors.at(0).desc;
+        outTensorDescs.at(0).dims.clear();
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(0));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(1));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(2) * inTensors.at(0).desc.dims.at(3));
+        outTensorDescs.at(1) = inTensors.at(1).desc;
+        outTensorDescs.at(2) = inTensors.at(2).desc;
+
+
+    } else {
+        // in : Q K V attention_mast pastK pastV [seq_len, batch, head_num, head_size]
+        // out : out presentK presentV [seq_len, batch, head_num * head_size]
+        outTensorDescs.at(0) = inTensors.at(0).desc;
+        outTensorDescs.at(0).dims.clear();
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(0));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(1));
+        outTensorDescs.at(0).dims.push_back(inTensors.at(0).desc.dims.at(2) * inTensors.at(0).desc.dims.at(3));
+        outTensorDescs.at(1) = inTensors.at(4).desc;
+        outTensorDescs.at(1).dims.at(0) = outTensorDescs.at(1).dims.at(0) + 1;
+        outTensorDescs.at(2) = inTensors.at(5).desc;
+        outTensorDescs.at(2).dims.at(0) = outTensorDescs.at(2).dims.at(0) + 1;
+    }
+    return AsdOps::Status::OkStatus();
+}
+
+RunnerBuilder *SelfAttentionCrossOperation::FindBestRunnerBuilder() const
+{
+#ifdef USE_TORCH_RUNNER
+    size_t index = AsdOps::GetSingleton<Config>().IsSelfAttentionCrossOpsRunnerEnable() ? 0 : 1;
+#else
+    size_t index = 0;
+#endif
+    return runnerBuilders_.at(index);
+}
+} // namespace AclTransformer
---
+++ core/ops/self_attention_cross/self_attention_cross_ops_runner_builder.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef SELFATTENTIONCROSS_OPS_RUNNER_BUILDER_H
+#define SELFATTENTIONCROSS_OPS_RUNNER_BUILDER_H
+#include <asdops/utils/log/log.h>
+#include "acltransformer/runner_builder.h"
+#include "acltransformer/params/self_attention_cross.h"
+#include <asdops/utils/singleton/singleton.h>
+#include "self_attention_cross_ops_llama7badapter_runner_310p.h"
+
+
+namespace AclTransformer {
+class SelfAttentionCrossOpsRunnerBuilder : public RunnerBuilder {
+public:
+    SelfAttentionCrossOpsRunnerBuilder(const SelfAttentionCrossParam &param) : param_(param) {}
+    virtual ~SelfAttentionCrossOpsRunnerBuilder() = default;
+    Runner *Build() override
+    {
+        if (param_.model == "llama_adapter") {
+            return new SelfAttentionCrossOpsLlama7bAdapterRunner310p(param_);
+        } else {
+            ASD_LOG(ERROR) << "invalid param_.model:" << param_.model;
+            return nullptr;
+        }
+    }
+
+private:
+    SelfAttentionCrossParam param_;
+};
+
+} // namespace AclTransformer
+#endif
