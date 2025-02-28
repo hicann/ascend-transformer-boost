c7ea4977d990c9e1ebca3ac42c75f547cfbdcf0b
wutong <1936739653@qq.com>
2025-02-28 03:26:42 +0000

    add
    Signed-off-by: wutong <1936739653@qq.com>

---
+++ src/include/atb/core/context_base.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#ifndef ATB_CONTEXT_BASE_H
+#define ATB_CONTEXT_BASE_H
+#include <memory>
+#include "atb/context.h"
+#include "atb/svector.h"
+#include "atb/core/tiling_buffer_pool/tiling_buffer_pool.h"
+#include "atb/core/runner_type.h"
+#include "atb/core/runner_pool.h"
+
+namespace atb {
+class ContextBase : public Context {
+public:
+    explicit ContextBase();
+    ~ContextBase() override;
+    ContextBase(const ContextBase &other) = delete;
+    ContextBase &operator=(const ContextBase &other) = delete;
+    Status Init();
+    void Destroy();
+    Status SetExecuteStream(aclrtStream stream) override;
+    aclrtStream GetExecuteStream() const override;
+    Status SetAsyncTilingCopyStatus(bool status) override;
+    bool GetAsyncTilingCopyStatus() const override;
+
+    aclrtStream GetAsyncTilingCopyStream() const;
+    aclrtEvent GetAsyncTilingCopyEvent();
+
+    uint8_t *GetHostTilingBuffer();
+    uint8_t *GetDeviceTilingBuffer();
+    uint64_t GetTilingBufferBlockSize() const;
+    RunnerPool &GetRunnerPool(RunnerType runnerType);
+    const Tensor &GetOverflowKernelOutTensor();
+
+private:
+    Status CreateCopyStreamAndEvents();
+    Status DestoryCopyStreamAndEvents();
+    Status CreateOverflowOutTensor();
+    Status FreeOverflowTensor();
+
+private:
+    aclrtStream executeStream_ = nullptr;
+
+    aclrtStream asyncTilingCopyStream_ = nullptr;
+    SVector<aclrtEvent> asyncTilingCopyEvents_;
+    size_t asyncTilingCopyEventsIndex_ = 0;
+
+    std::unique_ptr<TilingBufferPool> hostTilingBufferPool_;
+    std::unique_ptr<TilingBufferPool> deviceTilingBufferPool_;
+    std::vector<RunnerPool> runnerPools_;
+    Tensor overflowOutTensor_;
+};
+} // namespace atb
+#endif
