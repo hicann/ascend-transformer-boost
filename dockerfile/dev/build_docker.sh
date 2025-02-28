a6e87fd5f5cfeb0b2c87958991c33e009631f86f
wutong <1936739653@qq.com>
2025-02-28 01:55:12 +0000

    删除文件 dockerfile

--- dockerfile/dev/build_docker.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "/data/tmp" ];
-then
-    mkdir "/data/tmp"
-fi
-cp -r /data/tmp .
-docker build --no-cache --network=host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t acltransformer:v1 -f Dockerfile_dev .
-rm -rf tmp
