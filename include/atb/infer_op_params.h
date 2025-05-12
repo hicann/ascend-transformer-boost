bf6aece3a4327521bc0f9e3dd03e4dd4c0c917b3
Junc1 <hujuncheng3@huawei.com>
2025-05-12 03:48:56 +0000

    修改原因：MLA pa decoder op MTP场景支持lse输出
    Signed-off-by: Junc1 <hujuncheng3@huawei.com>

--- include/atb/infer_op_params.h
+++ include/atb/infer_op_params.h
@@ -2873,6 +2873,7 @@ struct MultiLatentAttentionParam {
         CALC_TYPE_UNDEFINED = 0, // 默认值
         CALC_TYPE_SPEC,          // 支持传入大于1的qseqlen
         CALC_TYPE_RING,          // ringAttention
+        CALC_TYPE_SPEC_AND_RING,          // 支持传入大于1的qseqlen ringAttention
     };
     //!
     //! \brief CalcType类型
