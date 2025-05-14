a460ebd16a9e44b3142d6e693c8ba04e15e0a8df
adamasW <wanghuimin45@h-partners.com>
2025-05-14 21:39:11 +0800

    modify

--- src/ops_infer/block_copy/block_copy_operation.cpp
+++ src/ops_infer/block_copy/block_copy_operation.cpp
@@ -34,7 +34,7 @@ template <> Status CreateOperation(const infer::BlockCopyParam &opParam, Operati
         return ERROR_INVALID_PARAM;
     }
     OP_PARAM_RSV_CHECK(opParam);
-    if (!GetSingleton<Config>().Is910B() && !GetSingleton<Config>().Is310p()) {
+    if (!GetSingleton<Config>().Is910B() && !GetSingleton<Config>().Is310P()) {
         ATB_LOG(ERROR) << "only support Atlas 800I A2 inference product and Atlas 300I Duo inference product";
         return ERROR_INVALID_PARAM;
     }
@@ -141,7 +141,7 @@ Status BlockCopyOperation::SetupDimCheck310P(const SVector<atb::Tensor> &inTenso
     if ((inTensors.at(0).desc.shape.dimNum != 4) ||  // kCache: 4 dim
         (inTensors.at(1).desc.shape.dimNum != 4) ||  // vCache: 4 dim
         (inTensors.at(2).desc.shape.dimNum != 1) ||  // 2: src
-        (inTensors.at(3).desc.shape.dimNum != 1) ||  // 3： dst
+        (inTensors.at(3).desc.shape.dimNum != 1) ||  // 3: dst
         (inTensors.at(4).desc.shape.dimNum != 1)  // 4: cumsum
     ) {
         ATB_LOG(ERROR) << "invalid intensor dimNums";
@@ -151,13 +151,14 @@ Status BlockCopyOperation::SetupDimCheck310P(const SVector<atb::Tensor> &inTenso
         inTensors.at(1).desc.format == aclFormat::ACL_FORMAT_FRACTAL_NZ) {
             if ((inTensors.at(0).desc.shape.dims[3] != NZBLOCKSIZE) ||
                 (inTensors.at(1).desc.shape.dims[3] != NZBLOCKSIZE)) {
-                    ATB_LOG(ERROR) << GetLogPrefix() << "NZ format tesnor dim should be aligned to 16";
+                    ATB_LOG(ERROR) << GetLogPrefix() << "NZ format tensor dim should be aligned to 16";
                 }
         }
     if (inTensors.at(2).desc.shape.dims[0] != inTensors.at(4).desc.shape.dims[0]) {
         ATB_LOG(ERROR) << "src dim should be same as cumsum";
         return ERROR_INVALID_TENSOR_DIM;
     }
+    return  NO_ERROR;
 }
 
 std::shared_ptr<Runner> BlockCopyOperation::CreateRunner(Context &context) const
