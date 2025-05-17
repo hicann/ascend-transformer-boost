9c1f69c5688aff3f9c9e6d9136c9de37797346b3
z00852506 <zhangyunsong3@huawei.com>
2025-05-17 14:50:07 +0800

    优化 mm 测试

--- tests/apitest/opstest/python/operations/mm_deq_swiglu_quant_mm_deq/test_mm_deq_swiglu_quant_mm_deq.py
+++ tests/apitest/opstest/python/operations/mm_deq_swiglu_quant_mm_deq/test_mm_deq_swiglu_quant_mm_deq.py
@@ -19,11 +19,29 @@ from precision_calcu import compare_cv
 
 OP_NAME = "MmDeqSwigluQuantMmDeqOperation"
 
+def matmul_dequant(x: torch.Tensor, weight: torch.Tensor, scale: torch.Tensor, per_token_scale: torch.Tensor,
+    calculate_dtype: torch.dtype, output_dtype: torch.dtype) -> torch.Tensor:
+    # shape 分析
+    m, k = x.shape
+    _, n = weight.shape
+
+    result = torch.matmul(x.to(torch.float32), weight.to(torch.float32)).to(calculate_dtype)
+    result *= scale[None, :].to(calculate_dtype)
+    result *= per_token_scale[:, None].to(calculate_dtype)
+    return result.to(output_dtype)
+
 def swiglu(x: torch.Tensor) -> torch.Tensor:
     x0, gate = x.chunk(2, dim=-1)
     swish = x0 * torch.sigmoid(x0)
     y = swish * gate
     return y
+
+def quant(x: torch.Tensor):
+    x_row_max = torch.max(torch.abs(x), dim=-1).values
+    quant_result = x * 127. / x_row_max[:, None]
+    y = torch.round(quant_result).to(torch.int8)
+    scale = (x_row_max / 127.).to(torch.float32)
+    return y, scale
 
 def permute_weight(w: torch.Tensor, tile_n: int):
     *dims, n = w.shape
@@ -33,9 +51,8 @@ def permute_weight(w: torch.Tensor, tile_n: int):
 def random_uniform(lower: float, upper: float, size: tuple, dtype: torch.dtype) -> torch.Tensor:
     return torch.rand(size=size, dtype=dtype) * (upper - lower) + lower
 
-# 真值计算
-def generate_data(m, n, k, calc_dtype=torch.float32):
-    n_out, n2, k2 = n // 2, k, n // 2
+def generate_data(m, n, k):
+    n2, k2 = k, n // 2
 
     # 数据生成
     x1 = torch.randint(-16, 16, size=(m, k), dtype=torch.int8)
@@ -44,55 +61,36 @@ def generate_data(m, n, k, calc_dtype=torch.float32):
     per_token_scale1 = random_uniform(0.004, 0.005, size=(m,), dtype=torch.float32)
     weight2 = torch.randint(-16, 16, size=(k2, n2,), dtype=torch.int8)
     scale2 = random_uniform(0.004, 0.005, size=(n2,), dtype=torch.float32)
-    # 中间参数
-    x2 = torch.empty(size=(m, k2), dtype=torch.int8)
-    per_token_scale2 = torch.empty(size=(m,), dtype=torch.float32)
 
-    # Grouped matmul dequant
-    matmul_result = torch.empty(size=(m, n), dtype=calc_dtype)
-    res = torch.matmul(x1.to(torch.float32), weight1.to(torch.float32)).to(calc_dtype)
-    res *= scale1[None, :].to(calc_dtype)
-    res *= per_token_scale1[:, None].to(calc_dtype)
-    matmul_result = res
+    # 计算真值
+    mm1_out = matmul_dequant(x1, weight1, scale1, per_token_scale1, torch.float64, torch.float64)
+    swiglu_out = swiglu(mm1_out)
+    x2, per_token_scale2 = quant(swiglu_out)
+    true_value = matmul_dequant(x2, weight2, scale2, per_token_scale2, torch.float64, torch.float64)
 
-    # 检查 matmul result
-    if not torch.isfinite(matmul_result.to(torch.float16)).all():
+    # 计算标杆
+    mm1_out = matmul_dequant(x1, weight1, scale1, per_token_scale1, torch.float32, torch.float16)
+    if not torch.isfinite(mm1_out.to(torch.float16)).all():
         return None
 
-    # Swiglu
-    swiglu_out = swiglu(matmul_result)
-
-    # 检查 swiglu_out
+    swiglu_out = swiglu(mm1_out.to(torch.float32))
     if not torch.isfinite(swiglu_out.to(torch.float16)).all():
         return None
 
-    # Quant
-    out_max = torch.max(torch.abs(swiglu_out), dim=-1).values
-    quant_result = swiglu_out * 127. / out_max[:, None]
-    x2 = torch.round(quant_result).to(torch.int8)
-    per_token_scale2 = (out_max / 127.).to(torch.float32)
-
-    # 检查 x2 和 per_token_scale2
+    x2, per_token_scale2 = quant(swiglu_out)
     if not torch.isfinite(x2).all() or not torch.isfinite(per_token_scale2).all():
         return None
 
-    # Grouped matmul dequant
-    true_value = torch.empty(size=(m, n2), dtype=calc_dtype)
-    res = torch.matmul(x2.to(torch.float32), weight2.to(torch.float32)).to(calc_dtype)
-    res *= scale2[None, :].to(calc_dtype)
-    res *= per_token_scale2[:, None].to(calc_dtype)
-    true_value = res
-
-    # 检查 true_value
-    if not torch.isfinite(true_value.to(torch.float16)).all():
+    golden = matmul_dequant(x2, weight2, scale2, per_token_scale2, torch.float32, torch.float16)
+    if not torch.isfinite(golden.to(torch.float16)).all():
         return None
 
-    return (x1, weight1, scale1, per_token_scale1, weight2, scale2, true_value)
+    return (x1, weight1, scale1, per_token_scale1, weight2, scale2, true_value, golden)
 
-def generate_data_safe(m, n, k, calc_dtype=torch.float32):
+def generate_data_safe(m, n, k):
     max_attempt_times = 5
     for _ in range(max_attempt_times):
-        data = generate_data(m, n, k, calc_dtype)
+        data = generate_data(m, n, k)
         if data is not None:
             return data
     raise ValueError(f"Try {max_attempt_times} times to generate data but still get illegal value.")
@@ -144,13 +142,10 @@ class TestMmDeqSwigluQuantMmDeq(operation_test.OperationTest):
         }
         tile_n = 256
 
-        m, n, k = 512, 4096, 7168
+        m, n, k = 32, 4096, 7168
 
         (x1, weight1, scale1, per_token_scale1, weight2, scale2,
-            self.true_value) = generate_data_safe(m, n, k)
-
-        self.golden = calculate_golden(x1.npu(), weight1.npu(), scale1.npu(), per_token_scale1.npu(),
-            weight2.npu(), scale2.npu())
+            self.true_value, self.golden) = generate_data_safe(m, n, k)
 
         in_tensors = [
             x1.npu(),
@@ -175,13 +170,10 @@ class TestMmDeqSwigluQuantMmDeq(operation_test.OperationTest):
         }
         tile_n = 256
 
-        m, n, k = 2464, 4096, 7168
+        m, n, k = 256, 4096, 7168
 
         (x1, weight1, scale1, per_token_scale1, weight2, scale2,
-            self.true_value) = generate_data_safe(m, n, k)
-
-        self.golden = calculate_golden(x1.npu(), weight1.npu(), scale1.npu(), per_token_scale1.npu(),
-            weight2.npu(), scale2.npu())
+            self.true_value, self.golden) = generate_data_safe(m, n, k)
 
         in_tensors = [
             x1.npu(),
@@ -206,13 +198,10 @@ class TestMmDeqSwigluQuantMmDeq(operation_test.OperationTest):
         }
         tile_n = 256
 
-        m, n, k = 2464, 4096, 7168
+        m, n, k = 576, 4096, 7168
 
         (x1, weight1, scale1, per_token_scale1, weight2, scale2,
-            self.true_value) = generate_data_safe(m, n, k)
-
-        self.golden = calculate_golden(x1.npu(), weight1.npu(), scale1.npu(), per_token_scale1.npu(),
-            weight2.npu(), scale2.npu())
+            self.true_value, self.golden) = generate_data_safe(m, n, k)
 
         in_tensors = [
             x1.npu(),
