e17b59e5242e8166b2304f62b5c87db4be989e6a
chenwei <chenwei542@huawei.com>
2025-05-19 20:03:55 +0800

    remove doxygen

--- src/include/atb/core/allocator/allocator.h
+++ src/include/atb/core/allocator/allocator.h
@@ -11,50 +11,12 @@
 #define ATB_ALLOCATOR_H
 #include "atb/types.h"
 
-//!
-//! \file allocator.h
-//!
-//! \brief 定义加速库内存管理类
-//!
-
-//!
-//! \namespace atb
-//!
-//! \brief 加速库的命名空间.
-//!
 namespace atb {
-
-//!
-//! \class Allocator.
-//!
-//! \brief 加速库Allocator类.
-//!
-//! 该接口类定义了加速库内存管理类，用于分配整图下发模式下的Device侧内存
-//!
 class Allocator {
 public:
-    //! \brief 默认构造函数.
     Allocator() = default;
-
-    //! \brief 默认析构函数.
     virtual ~Allocator() = default;
-
-    //!
-    //! \brief 分配一块bufferSize大小的Device侧内存，返回分配好的内存首地址
-    //!
-    //! \param bufferSize 需要申请的内存大小
-    //!
-    //! \return 返回申请出来的指针
-    //!
     virtual void* Allocate(size_t bufferSize) = 0;
-
-    //!
-    //! \brief 回收已经分配过的Device侧内存
-    //!
-    //! \param addr 需要释放的内存地址
-    //!
-    //! \return 状态值.如果释放成功，返回NO_ERROR
-    //!
     virtual Status Deallocate(void *addr) = 0;
 };
 } // namespace atb
