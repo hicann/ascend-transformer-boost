547d803ab51d38e38e1a5168a0f664e1519726a4
dovahkiiin <haorunzhe@h-partners.com>
2025-05-19 17:58:14 +0800

    param to jason fix v2

--- ops_customize/include/param_to_jason.h
+++ ops_customize/include/param_to_jason.h
@@ -1,5 +1,6 @@
 #ifndef CUSTOMIZE_PARAM_TO_JSON_H
 #define CUSTOMIZE_PARAM_TO_JSON_H
+#include <nlohmann/json.hpp>
 namespace atb {
 template <typename OpParam> nlohmann::json OpParamToJson(const OpParam &opParam);
 }
