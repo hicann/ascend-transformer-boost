e26805716f224c10e8a36246e4824b83e1ce3552
dovahkiiin <haorunzhe@h-partners.com>
2025-05-20 11:45:11 +0800

    example tests added not varified

---
+++ ops_customize/ops/example_op/tests/example_op_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "example_operation.h"
+#include "customize_op_params.h"
+#include "atb/core/op_param_funcs.h"
+
+using namespace atb;
+
+TEST(ExampleOpTest, CreateOperation_Success) {
+    customize::AddParam param;
+    param.paramForNothing = 0;
+
+    Operation *op = nullptr;
+    Status st = CreateOperation(param, &op);
+    EXPECT_EQ(st, NO_ERROR) << "Create op should return NO_ERROR";
+    ASSERT_NE(op, nullptr) << "op can not be nullptr";
+
+    delete op;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUNN_ALL_TEST();
+}
