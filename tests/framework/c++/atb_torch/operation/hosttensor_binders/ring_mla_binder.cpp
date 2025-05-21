1d4cb2524305739bafcfdbc49c710732a89feb40
ivanshan_8170 <shanzidan@h-partners.com>
2025-05-21 17:27:30 +0000

    update tests/framework/c++/atb_torch/operation/hosttensor_binders/ring_mla_binder.cpp.
    Signed-off-by: ivanshan_8170 <shanzidan@h-partners.com>

--- tests/framework/c++/atb_torch/operation/hosttensor_binders/ring_mla_binder.cpp
+++ tests/framework/c++/atb_torch/operation/hosttensor_binders/ring_mla_binder.cpp
@@ -38,30 +38,3 @@ void RingMLABinder::BindTensor(atb::VariantPack &variantPack)
     ATB_LOG(INFO) << "Ring MLA BindTensor";
     variantPack.inTensors.at(SEQLEN_INDEX).hostData = seqLen_.data();
 }
- +26 tests/framework/c++/atb_torch/operation/hosttensor_binders/ring_mla_binder.h  0 -> 100644
-/*
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
-#ifndef ATB_RING_MLA_BINDER_H
-#define ATB_RING_MLA_BINDER_H
-#include "hosttensor_binder.h"
-#include <vector>
-
-class RingMLABinder : public HostTensorBinder {
-public:
-    RingMLABinder();
-    virtual ~RingMLABinder();
-    void ParseParam(const nlohmann::json &paramJson) override;
-    void BindTensor(atb::VariantPack &variantPack) override;
-
-private:
-    std::vector<int32_t> seqLen_;
-};
-
-#endif // ATB_RING_MLA_BINDER_H
