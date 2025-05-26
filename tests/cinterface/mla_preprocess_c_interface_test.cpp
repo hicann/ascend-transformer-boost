16e9ae36dbe9119b12cfb1cf1fcbc7b63a26e0a4
x30073543 <xiaohanjun3@h-partners.com>
2025-05-26 15:50:35 +0800

    mla&& preprocess only supprot 910b

--- tests/cinterface/mla_preprocess_c_interface_test.cpp
+++ tests/cinterface/mla_preprocess_c_interface_test.cpp
@@ -1,9 +1,14 @@
 #include "c_interface_utils.h"
+#include "atb/utils/config.h"
+#include "atb/utils/singleton.h"
 using namespace atb;
 using namespace atb::cinterfaceTest;
 
 TEST(TestATBACL, TestMLAPreProcesscomb0Q0C0)
 {
+    if (!GetSingleton<Config>().Is910B()) {
+        exit(0);        
+    }
     atb::Context *context = nullptr;
     aclrtStream stream = nullptr;
     int64_t deviceId = 0;
