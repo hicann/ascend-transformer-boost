f43bce64f659cab0c7545fa7a74833f3627c4bfe
chenwei <chenwei542@huawei.com>
2025-05-28 16:39:31 +0800

    remove useless code

--- src/atb/core/tiling_buffer_pool/tiling_buffer_pool.cpp
+++ src/atb/core/tiling_buffer_pool/tiling_buffer_pool.cpp
@@ -7,7 +7,6 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
-#include <functional>
 #include "atb/core/tiling_buffer_pool/tiling_buffer_pool.h"
 #include "atb/utils/log.h"
 
