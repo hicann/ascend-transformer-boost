a79df79d4815ff45564d3ef93a30412dfd806c81
sum39c5bb <zouyanlong@h-partners.com>
2025-05-28 17:19:13 +0800

    fix

--- tests/apitest/opstest/python/operations/sort/test_sort_updateparam_performance.py
+++ tests/apitest/opstest/python/operations/sort/test_sort_updateparam_performance.py
@@ -7,6 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 #
+import operation_test
 import sys
 import os
 import json
@@ -22,20 +23,20 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-import operation_test
 
 OP_NAME = "SortOperation"
 
 
-class TestSortOperation(operation_test.OperationTest):    
+class TestSortOperation(operation_test.OperationTest):
 
     def test_3d_float(self):
-        if  operation_test.get_soc_version() == 'Ascend910A' or \
-            operation_test.get_soc_version() == 'Ascend310B':
+        if operation_test.get_soc_version() == 'Ascend910A' or \
+                operation_test.get_soc_version() == 'Ascend310B':
             logging.info("this testcase don't supports Ascend910A\Ascend310B")
             return True
 
-        intensor = torch.randint(-65504, 65504, (8192, 8192, 8192)).float().npu().half()
+        intensor = torch.randint(-65504, 65504,
+                                 (512, 512, 8192)).float().npu().half()
 
         operation = torch.classes.OperationTorch.OperationTorch(OP_NAME)
 
@@ -43,29 +44,43 @@ class TestSortOperation(operation_test.OperationTest):
         createOperation_start_time = time.perf_counter()
         operation.set_param(json.dumps(PARAM))
         createOperation_end_time = time.perf_counter()
-        print(f"createOperation time: {createOperation_end_time - createOperation_start_time} second")
+        print(
+            f"createOperation time: {createOperation_end_time - createOperation_start_time} second")
 
-        execute_start_time = time.perf_counter()
-        operation.execute([intensor])
-        execute_end_time = time.perf_counter()
-        print(f"execute time: {execute_end_time - execute_start_time} second")
+        sum = 0
+        for i in range(100):
+            execute_start_time = time.perf_counter()
+            operation.execute([intensor])
+            execute_end_time = time.perf_counter()
+            sum += execute_end_time - execute_start_time
 
-        PARAM = {"num": [8191]}
-        updateParam_start_time = time.perf_counter()
-        operation.update_param(json.dumps(PARAM))
-        updateParam_end_time = time.perf_counter()
-        print(f"updateParam time: {updateParam_end_time - updateParam_start_time} second")
+        print(f"execute average time: {sum / 100} second")
+
+        operation = torch.classes.OperationTorch.OperationTorch(OP_NAME)
+
+        PARAM = {"num": [4096]}
+        createOperation_start_time = time.perf_counter()
+        operation.set_param(json.dumps(PARAM))
+        createOperation_end_time = time.perf_counter()
+        print(
+            f"createOperation time: {createOperation_end_time - createOperation_start_time} second")
 
         PARAM = {"num": [8192]}
         updateParam_start_time = time.perf_counter()
         operation.update_param(json.dumps(PARAM))
         updateParam_end_time = time.perf_counter()
-        print(f"updateParam time: {updateParam_end_time - updateParam_start_time} second")
+        print(
+            f"updateParam time: {updateParam_end_time - updateParam_start_time} second")
 
-        execute_start_time = time.perf_counter()
-        operation.execute([intensor])
-        execute_end_time = time.perf_counter()
-        print(f"execute time: {execute_end_time - execute_start_time} second")
+        sum = 0
+        for i in range(100):
+            execute_start_time = time.perf_counter()
+            operation.execute([intensor])
+            execute_end_time = time.perf_counter()
+            sum += execute_end_time - execute_start_time
+
+        print(f"execute average time: {sum / 100} second")
+
 
 if __name__ == '__main__':
     unittest.main()
