d4c41970bde618abac9fbf796bef61882cc0f8b8
ivanshan_8170 <shanzidan@h-partners.com>
2025-06-03 16:52:24 +0800

    temp fix

--- src/ops_infer/ring_mla/ring_mla_ops_runner.cpp
+++ src/ops_infer/ring_mla/ring_mla_ops_runner.cpp
@@ -67,10 +67,9 @@ RingMLAOpsRunner::RingMLAOpsRunner(const infer::RingMLAParam &param)
     RingMLANode.inTensors = {query_split1, query_split2, key_split1,   key_split2,   value,
                              mask,         &nullTensor_, &nullTensor_, &nullTensor_, &nullTensor_,
                              &nullTensor_, &nullTensor_, &nullTensor_, prevOut,      prevLse};
-    SVector<Mki::Tensor> intermidateOutTensors = {prevOut, prevLse};
-    TensorUtil::FastCopyTensors(intermidateOutTensors, runnerVariantPack_.outTensors);
+    SVector<Mki::Tensor> intermidateOutTensors = {&prevOut, &prevLse};
+    TensorUtil::FastCopyTensors(intermidateOutTensors, RingMLANode.outTensors);
 
-    RingMLANode.outTensors = {attnOut, softmaxLse};
     RingMLANode.inTensorViewFuncs.resize(RingMLANode.inTensors.size()); // view
     RingMLANode.inferShapePreFunc = [](Mki::LaunchParam &launchParam) { // format, dtype 设置
         for (size_t i = 0; i < launchParam.GetInTensorCount(); i++) {
