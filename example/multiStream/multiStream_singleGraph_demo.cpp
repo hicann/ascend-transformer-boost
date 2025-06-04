49c6bfa18acc7ef5fbbe10536166ac0c9e9564a3
chenwei <chenwei542@huawei.com>
2025-06-04 15:33:13 +0800

    add demo and readme

--- example/multiStream/multiStream_singleGraph_demo.cpp
+++ example/multiStream/multiStream_singleGraph_demo.cpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <atb/utils.h>
 #include <unistd.h>
-#include <acl/acl_mdl.h>
-#include <cstdlib>  // 包含 std::getenv
-#include <cstring>  // 包含 std::strcmp（用于字符串比较）
 
 static void CreateInTensorDescs(atb::SVector<atb::TensorDesc> &intensorDescs) 
 {
@@ -122,7 +119,7 @@ static void CreateGraphOperationForMultiStream(atb::GraphParam &opGraph, atb::Op
     atb::CreateOperation(opGraph, operation);
 }
 
-static void CreateGraphOperationWithEvent(atb::GraphParam &opGraph, atb::Operation **operation, aclrtEvent event)
+static void CreateGraphOperationWithWREvent(atb::GraphParam &opGraph, atb::Operation **operation, aclrtEvent event)
 {
     opGraph.inTensorNum = 2;
     opGraph.outTensorNum = 1;
@@ -168,27 +165,55 @@ static void CreateGraphOperationWithEvent(atb::GraphParam &opGraph, atb::Operati
     atb::CreateOperation(opGraph, operation);
 }
 
-// 打印结果
-void PrintOutTensorValue(atb::Tensor &outTensor)
+static void CreateGraphOperationWithRWEvent(atb::GraphParam &opGraph, atb::Operation **operation, aclrtEvent event)
 {
-    std::vector<uint16_t> outBuffer(atb::Utils::GetTensorNumel(outTensor));
-    int ret = aclrtMemcpy(outBuffer.data(), outBuffer.size() * sizeof(uint16_t), outTensor.deviceData, outTensor.dataSize, ACL_MEMCPY_DEVICE_TO_HOST);
-    if (ret != 0) {
-        std::cout << "copy error!";
-        exit(0);
-    }
-    for (size_t i = 0; i < outBuffer.size(); i = i + 1) {
-        std::cout << "out[" << i << "] = " << (uint32_t)outBuffer.at(i) << std::endl;
-    }
+    opGraph.inTensorNum = 2;
+    opGraph.outTensorNum = 1;
+    opGraph.internalTensorNum = 2;
+    opGraph.nodes.resize(5);
+
+    size_t nodeId = 0;
+    atb::Node &mulNode = opGraph.nodes.at(nodeId++);
+    atb::Node &waitNode = opGraph.nodes.at(nodeId++);
+    atb::Node &addNode = opGraph.nodes.at(nodeId++);
+    atb::Node &graphNode = opGraph.nodes.at(nodeId++);
+    atb::Node &recordNode = opGraph.nodes.at(nodeId++);
+
+    atb::infer::ElewiseParam mulParam;
+    mulParam.elewiseType = atb::infer::ElewiseParam::ElewiseType::ELEWISE_MUL;
+    atb::CreateOperation(mulParam, &mulNode.operation);
+    mulNode.inTensorIds = {0, 1};
+    mulNode.outTensorIds = {3};
+
+    atb::common::EventParam waitParam;
+    waitParam.event = event;
+    waitParam.operatorType = atb::common::EventParam::OperatorType::RECORD;
+    atb::CreateOperation(waitParam, &waitNode.operation);
+
+    atb::GraphParam graphParam;
+    CreateMiniGraphOperation(graphParam, &graphNode.operation);
+    graphNode.inTensorIds = {3, 4};
+    graphNode.outTensorIds = {2};
+
+    atb::infer::ElewiseParam addParam;
+    addParam.elewiseType = atb::infer::ElewiseParam::ElewiseType::ELEWISE_ADD;
+    atb::CreateOperation(addParam, &addNode.operation);
+    addNode.inTensorIds = {0, 1};
+    addNode.outTensorIds = {4};
+    SetExecuteStreamId(addNode.operation, 1);
+
+    atb::common::EventParam recordParam;
+    recordParam.event = event;
+    recordParam.operatorType = atb::common::EventParam::OperatorType::WAIT;
+    atb::CreateOperation(recordParam, &recordNode.operation);
+    SetExecuteStreamId(recordNode.operation, 1);
+
+    atb::CreateOperation(opGraph, operation);
 }
 
 int main()
 {
     aclInit(nullptr);
-    // 使用环境变量ATB_MULTISTREAM_TYPE判断使用单图多流还是多图多流功能
-    // ATB_MULTISTREAM_TYPE=singleGraph ：单图多流
-    // ATB_MULTISTREAM_TYPE=MultiGraph ：多图多流
-    const char* multiStreamType = std::getenv("ATB_MULTISTREAM_TYPE");
 	// 设置卡号、创建stream、创建context、设置stream
     uint32_t deviceId = 1;
     aclrtSetDevice(deviceId);
@@ -202,7 +227,7 @@ int main()
     atb::Context *context = nullptr;
     atb::CreateContext(&context);
     context->SetExecuteStreams(streams);
-    // 创建图算子
+    // 创建图内多流并行Op
     atb::Operation *operation = nullptr;
     atb::GraphParam opGraph;
     CreateGraphOperationForMultiStream(opGraph, &operation);
@@ -231,35 +256,29 @@ int main()
     uint64_t workspaceSize = 0;
     void *workSpace = nullptr;
     int ret = 0;
-    // 图执行
-    if (!multiStreamType || std::strcmp(multiStreamType, "singleGraph") == 0) {
-        // 图内多流并行
-        std::cout << "Single graph multi-stream demo start" << std::endl;
-        CreateInTensors(pack.inTensors, intensorDescs);
-        operation->Setup(pack, workspaceSize, context);
-        if (workspaceSize != 0 && workSpace == nullptr) {
-            ret = aclrtMalloc(&workSpace, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
-            if (ret != 0) {
-                std::cout << "alloc error!\n";
-                exit(0);
-            }
-        }
-        operation->Execute(pack, (uint8_t*)workSpace, workspaceSize, context);
-        //流同步
-        ret = aclrtSynchronizeStream(stream1);
+    // 图内多流并行
+    std::cout << "Single graph multi-stream demo start" << std::endl;
+    CreateInTensors(pack.inTensors, intensorDescs);
+    operation->Setup(pack, workspaceSize, context);
+    if (workspaceSize != 0 && workSpace == nullptr) {
+        ret = aclrtMalloc(&workSpace, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
         if (ret != 0) {
-            std::cout << "sync error!";
+            std::cout << "alloc error!\n";
             exit(0);
         }
+    }
+    operation->Execute(pack, (uint8_t*)workSpace, workspaceSize, context);
+    //流同步
+    ret = aclrtSynchronizeStream(stream1);
+    if (ret != 0) {
+        std::cout << "sync error!";
+        exit(0);
+    }
 
-        ret = aclrtSynchronizeStream(stream2);
-        if (ret != 0) {
-            std::cout << "sync error!";
-            exit(0);
-        }
-    } else {
-        // 图间同步
-        std::cout << "multi graph multi-stream demo start" << std::endl;
+    ret = aclrtSynchronizeStream(stream2);
+    if (ret != 0) {
+        std::cout << "sync error!";
+        exit(0);
     }
 
 	// 资源释放
