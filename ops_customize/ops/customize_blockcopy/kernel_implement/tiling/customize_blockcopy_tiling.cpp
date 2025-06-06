6a7b16d65cc0c6cf2ef7d0c004fe3254b1fce435
dovahkiiin <haorunzhe@h-partners.com>
2025-06-06 14:37:27 +0800

    fix include path and dim0

--- ops_customize/ops/customize_blockcopy/kernel_implement/tiling/customize_blockcopy_tiling.cpp
+++ ops_customize/ops/customize_blockcopy/kernel_implement/tiling/customize_blockcopy_tiling.cpp
@@ -13,7 +13,7 @@
 #include <mki/utils/log/log.h>
 #include <mki/utils/math/math.h>
 #include <mki/utils/platform/platform_info.h>
-#include "atbops/params/params.h"
+#include "customize_blockcopy/kernel_implement/include/customizeblockcopy.h"
 #include "tiling_data.h"
 static constexpr int32_t NZ_SIZE = 16;
 static constexpr uint64_t BLOCKCOPY_WKSP_TENSOR_IDX = 7;
@@ -37,13 +37,13 @@ bool BlockCopyTilingNd(const LaunchParam &launchParam, KernelInfo &kernelInfo)
     CustomizeBlockCopyTilingData *tilingDataPtr =
         reinterpret_cast<AtbOps::CustomizeBlockCopyTilingData *>(kernelInfo.GetTilingHostAddr());
     MKI_CHECK(tilingDataPtr != nullptr, "tilingHost should not be empty", return false);
-    auto &kShape = launchParam.GetInTensor(DIM_0).desc.dims;
-    auto &vShape = launchParam.GetInTensor(DIM_1).desc.dims;
+    auto &kShape = launchParam.GetInTensor(0).desc.dims;
+    auto &vShape = launchParam.GetInTensor(1).desc.dims;
     // 910B kv shape [#B, B, head, DIM]
-    uint32_t blockSize = static_cast<uint32_t>(kShape.at(DIM_1));
-    uint32_t numHead = static_cast<uint32_t>(kShape.at(DIM_2));
-    uint32_t headSizeK = static_cast<uint32_t>(kShape.at(DIM_3));
-    uint32_t headSizeV = static_cast<uint32_t>(vShape.at(DIM_3));
+    uint32_t blockSize = static_cast<uint32_t>(kShape.at(1));
+    uint32_t numHead = static_cast<uint32_t>(kShape.at(2));
+    uint32_t headSizeK = static_cast<uint32_t>(kShape.at(3));
+    uint32_t headSizeV = static_cast<uint32_t>(vShape.at(3));
     MKI_CHECK(blockSize > 0, "blockSize is invalid", return false);
     MKI_CHECK(numHead > 0, "numHead is invalid", return false);
     tilingDataPtr->blockSize = blockSize;
@@ -70,15 +70,15 @@ bool BlockCopyTilingNz(const LaunchParam &launchParam, KernelInfo &kernelInfo)
     CustomizeBlockCopyTilingData *tilingDataPtr =
         reinterpret_cast<AtbOps::CustomizeBlockCopyTilingData *>(kernelInfo.GetTilingHostAddr());
     MKI_CHECK(tilingDataPtr != nullptr, "tilingHost should not be empty", return false);
-    auto &kShape = launchParam.GetInTensor(DIM_0).desc.dims;
-    auto &vShape = launchParam.GetInTensor(DIM_1).desc.dims;
+    auto &kShape = launchParam.GetInTensor(0).desc.dims;
+    auto &vShape = launchParam.GetInTensor(1).desc.dims;
     // nz：无法获取头和头大小；numhead弃置，改用headSizeV来保存大小
     // 310p的kv：[#B, head*head_size // 16, B, 16]
-    uint32_t blockSize = static_cast<uint32_t>(kShape.at(DIM_2));
+    uint32_t blockSize = static_cast<uint32_t>(kShape.at(2));
     // 头参数固定设置为1
     uint32_t numHead = static_cast<uint32_t>(1);
-    uint32_t headSizeK = static_cast<uint32_t>(kShape.at(DIM_1) * NZ_SIZE);
-    uint32_t headSizeV = static_cast<uint32_t>(vShape.at(DIM_1) * NZ_SIZE);
+    uint32_t headSizeK = static_cast<uint32_t>(kShape.at(1) * NZ_SIZE);
+    uint32_t headSizeV = static_cast<uint32_t>(vShape.at(1) * NZ_SIZE);
     MKI_CHECK(blockSize > 0, "blockSize is invalid", return false);
     MKI_CHECK(numHead > 0, "numHead is invalid", return false);
     MKI_CHECK(headSizeK > 0, "headSizeK is invalid", return false);
@@ -116,14 +116,14 @@ bool BlockCopyTilingCheck310P(const LaunchParam &launchParam, KernelInfo &kernel
 Status CustomizeBlockCopyTiling(const LaunchParam &launchParam, KernelInfo &kernelInfo)
 {
     auto param = AnyCast<OpParam::CustomizeBlockCopy>(launchParam.GetParam());
-    auto &kShape = launchParam.GetInTensor(DIM_0).desc.dims;
-    auto &srcShape = launchParam.GetInTensor(DIM_2).desc.dims;
-    auto &dstShape = launchParam.GetInTensor(DIM_3).desc.dims;
-    auto &cumShape = launchParam.GetInTensor(DIM_4).desc.dims;
-    uint32_t blockCount = static_cast<uint32_t>(kShape.at(DIM_0));
-    uint32_t sourceCount = static_cast<uint32_t>(srcShape.at(DIM_0));
-    uint32_t destinationCount = static_cast<uint32_t>(dstShape.at(DIM_0));
-    uint32_t cumSumCount = static_cast<uint32_t>(cumShape.at(DIM_0));
+    auto &kShape = launchParam.GetInTensor(0).desc.dims;
+    auto &srcShape = launchParam.GetInTensor(2).desc.dims;
+    auto &dstShape = launchParam.GetInTensor(3).desc.dims;
+    auto &cumShape = launchParam.GetInTensor(4).desc.dims;
+    uint32_t blockCount = static_cast<uint32_t>(kShape.at(0));
+    uint32_t sourceCount = static_cast<uint32_t>(srcShape.at(0));
+    uint32_t destinationCount = static_cast<uint32_t>(dstShape.at(0));
+    uint32_t cumSumCount = static_cast<uint32_t>(cumShape.at(0));
 
     if (param.type == OpParam::CustomizeBlockCopy::BLOCK_COPY_CACHE_ND) {
         MKI_CHECK(BlockCopyTilingNd(launchParam, kernelInfo), "K/V Cache size is invalid",
