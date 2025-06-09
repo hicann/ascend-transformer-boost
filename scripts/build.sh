8a311ccd55a6de2797ad3d3a3eec77dd2130a97a
dovahkiiin <haorunzhe@h-partners.com>
2025-06-09 21:16:06 +0800

    tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 #
-
+#hahaha
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/..
