7cb50b8337c6034e9be5a7f5ea0833906021d24c
guangdongzhu <root@localhost.localdomain>
2025-07-02 15:31:45 +0800

    Revert "buffersize back"
    This reverts commit f19fc392b00d6b078fb1ba0b7553fa1e5fc35644.

--- src/kernels/include/lcal/lcal_comm.h
+++ src/kernels/include/lcal/lcal_comm.h
@@ -26,6 +26,7 @@ class LcalSockExchange;
 class LcalComm {
 public:
     LcalComm(int rank, int rankSize);
+    LcalComm(int rank, int rankSize, int bufferSize);
     LcalComm(int rank, int rankSize, int commDomain, int bufferSize, int isEnableMagic);
     LcalComm(int rank, int rankSize, LcalUniqueId commId);
     ~LcalComm();
@@ -36,6 +37,7 @@ public:
     int GetRank() const;
     int GetRankSize() const;
     int GetCommSize() const;
+    int GetBufferSize() const;
     const PhysicalInfo &GetPhysicalInfo() const;
     GM_ADDR GetCommArgsPtr() const;
     CommArgs* GetCommArgs();
