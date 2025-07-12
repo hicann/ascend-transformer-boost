51b3fe6d6591943d13190762cd3d6688c81a7941
dovahkiiin <haorunzhe@h-partners.com>
2025-07-12 15:51:36 +0800

    case fix and copyright add

--- tests/apitest/torch_atb_test/op_test/linear_parallel/matmul_allreduce_pertoken_test.py
+++ tests/apitest/torch_atb_test/op_test/linear_parallel/matmul_allreduce_pertoken_test.py
@@ -1,3 +1,13 @@
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This file is a part of the CANN Open Software.
+# Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+#
+
 import torch
 import torch_atb
 import torch_npu
@@ -40,7 +50,6 @@ def run_test(rank, size):
     logging.info(f'Process {rank} started, using device npu:{rank}.')
     torch.manual_seed(random_seed)
 
-    # m, k, n = 57600, 6144, 6144
     m, k, n = 2, 2, 2
     inTensors_npu = []
     for _ in range(rank_size):
@@ -58,16 +67,14 @@ def run_test(rank, size):
     logging.info(f"shape of npu_outputs for rank {rank}: {npu_res_cur_rank.shape}")
     logging.debug(f"npu_outputs for rank {rank}: {npu_res_cur_rank}")
 
-    # assert check_precision(npu_res_cur_rank, cpu_res_cur_rank, rank), "Test failed"
-
-class TestLinearParallel(unittest.TestCase):
+class TestMatmulAllreducePertoken(unittest.TestCase):
     def test(self):
         if not is910B():
             print("This test case only supports 910B")
             return True
-        print("----------- linear_parallel test begin ------------")
+        print("----------- matmul_allreduce_pertoken test begin ------------")
         mp.spawn(run_test, nprocs=rank_size, args=(rank_size,))
-        print("----------- linear_parallel test success ------------")
+        print("----------- matmul_allreduce_pertoken test success ------------")
 
 if __name__ == "__main__":
     unittest.main()
