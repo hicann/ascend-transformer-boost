4b7f7a8fa0f4ec5af915301038787566265f37e6
He Changcheng <hcc2303@163.com>
2025-08-18 20:18:19 +0800

    draft

--- comm/lcal/src/lcal_internal.cpp
+++ comm/lcal/src/lcal_internal.cpp
@@ -30,7 +30,7 @@ LCAL_CCE_BIN_STR:.incbin "/tmp/lcal_cce.o"
 constexpr int LCCL_RT_DEV_BINARY_MAGIC_ELF_AIVEC = 0x41415246;
 constexpr int COC_RT_DEV_BINARY_MAGIC_ELF = 0x43554245;
 
-namespcae Lcal {
+namespace Lcal {
 const std::map<HcclDataType, std::string> DATATYPE2NAME = {
     {HCCL_DATA_TYPE_INT32, "int"},
     {HCCL_DATA_TYPE_INT16, "int16_t"},
@@ -38,13 +38,97 @@ const std::map<HcclDataType, std::string> DATATYPE2NAME = {
     {HCCL_DATA_TYPE_INT64, "int64_t"},
     {HCCL_DATA_TYPE_FP32, "float"},
     {HCCL_DATA_TYPE_FP16, "float16_t"},
-    {HCCL_DATA_TYPE_BFP16, "bfloat16_t"},
+    {HCCL_DATA_TYPE_BFP16, "bfloat16_t"}
 };
 
 
 template<class T>
-int RegisterBinaryKernel()
+int RegisterBinaryKernel(const string &funcName, int8_t *funSig, const T *binStrPtr, int magic, int len = 0)
+{
+    rtDevBinary_t binary;
+    void *binHandle = nullptr;
+    binary.data = binStrPtr;
+    binary.length = (len == 0 ? LCAL_1OP_BIN_SIZE : len);
 
+    binary.magic = magic;
+    binary.version = 0;
+    rtError_t rtRet = rtDevBinaryRegister(&binary, &binHandle);
+    if (rtRet != RT_ERROR_NONE) {
+        MKI_LOG(WARN) << "rtDevBinaryRegister failed! " << to_string(rtRet) << ", funcName = " << funcName;
+        return LCAL_ERROR_INTERNAL;
+    }
+    rtRet = rtFunctionRegister(binHandle, funSig, funcName.c_str(), funcName.c_str(), 0);
+    if (rtRet != RT_ERROR_NONE) {
+        MKI_LOG(WARN) << "rtFunctionRegister failed! " << to_string(rtRet) << ", funcName = " << funcName;
+        return LCAL_ERROR_INTERNAL;
+    }
+    return LCAL_SUCCESS;
+}
+
+int8_t *GetFunSig(LcalType type, HcclDataType dataType, uint64_t devType = 0)
+{
+    constexpr int sigOffset = 16;
+    constexpr int sigSkew = 0x1000;
+    return reinterpret_cast<int8_t *>((static_cast<uint64_t>(type) << sigOffset << sigOffset) +
+        (static_cast<uint64_t>(dataType)<< sigOffset) + devType + sigSkew);
+}
+
+
+const int* FindNextOpStart(const int opStartMagic, const int* cclBinEndPtr, const int* cclBinPtr)
+{
+    if (cclBinPtr == nullptr) {
+        MKI_LOG(ERROR) << "FindNextOpStart failed! cclBinPtr is nullptr";
+        return nullptr;
+    }
+    while (*cclBinPtr != opStartMagic and cclBinPtr < cclBinEndPtr) {
+        cclBinPtr++;
+    }
+    if (*cclBinPtr == opStartMagic) {
+        cclBinPtr++;
+    }
+    return cclBinPtr;
+}
+
+int RegistCCLOp2Kernel(const int* cclBinPtr, const int* nextPtr)
+{
+    vector<HcclDataType> registerTypes = { HCCL_DATA_TYPE_INT32, HCCL_DATA_TYPE_INT16, HCCL_DATA_TYPE_INT8,
+                                           HCCL_DATA_TYPE_FP32,  HCCL_DATA_TYPE_FP16, HCCL_DATA_TYPE_BFP16,
+                                           HCCL_DATA_TYPE_INT64 };
+    std::vector<LcalType> registerCCLTypesOp2 = {
+        LcalType::ALL_GATHER, LcalType::REDUCE_SCATTER, LcalType::ALL2ALL,
+    };
+    int res = LCAL_SUCCESS;
+    for (auto ccl : registerCCLTypesOp2) {
+        for (auto t : registerTypes) {
+            res = RegisterBinaryKernel(LCAL_TYPE2NAME.at(ccl) + "_" + DATATYPE2NAME.at(t), GetFunSig(ccl, t),
+                cclBinPtr, LCCL_RT_DEV_BINARY_MAGIC_ELF_AIVEC, (nextPtr - cclBinPtr) * sizeof(int));
+        }
+    }
+    if (res != LCAL_SUCCESS) {
+        return res;
+    }
+    res = RegisterBinaryKernel(LCAL_TYPE2NAME.at(LcalType::BROADCAST),
+        GetFunSig(LcalType::BROADCAST, HCCL_DATA_TYPE_RESERVED), cclBinPtr, LCCL_RT_DEV_BINARY_MAGIC_ELF_AIVEC);
+    return res;
+}
+
+int RegistCCLOp1Kernel(const int* cclBinPtr, const int* nextPtr)
+{
+    vector<HcclDataType> registerTypes = { HCCL_DATA_TYPE_INT32, HCCL_DATA_TYPE_INT16, HCCL_DATA_TYPE_INT8,
+                                           HCCL_DATA_TYPE_FP32,  HCCL_DATA_TYPE_FP16, HCCL_DATA_TYPE_BFP16,
+                                           HCCL_DATA_TYPE_INT64 };
+    std::vector<LcalType> registerCCLTypesOp1 = {
+        LcalType::ALL_REDUCE,
+    };
+    int res = LCAL_SUCCESS;
+    for (auto ccl : registerCCLTypesOp1) {
+        for (auto t : registerTypes) {
+            res = RegisterBinaryKernel(LCAL_TYPE2NAME.at(ccl) + "_" + DATATYPE2NAME.at(t), GetFunSig(ccl, t),
+                cclBinPtr, LCCL_RT_DEV_BINARY_MAGIC_ELF_AIVEC, (nextPtr - cclBinPtr) * sizeof(int));
+        }
+    }
+    return res;
+}
 
 
 }
