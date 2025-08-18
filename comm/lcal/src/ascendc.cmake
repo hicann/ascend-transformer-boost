86e25c70cddb05eca33c111b7313490aaee35261
LiuHaoyu <liuhaoyu35@huawei.com>
2025-08-18 09:42:37 +0800

    ascendc cmake 2nd

--- comm/lcal/src/ascendc.cmake
+++ comm/lcal/src/ascendc.cmake
@@ -8,34 +8,37 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 #
 enable_language(CCE)
+# 设置编译选项
+# 定义 sanitizer 相关的编译选项，如果启用则添加，否则为空
 if(USE_MSSANITIZER)
     set(SANITIZER_FLAGS
-        -g --ce-enable-sanitizer
+        -g --cce-enable-sanitizer
     )
     set(SANITIZER_DEPEND_LIBS
         --dependent-libraries ${ASCEND_HOME_PATH}/tools/mssanitizer/lib64/libsanitizer_stub_dav-c220-vec.a
         --dependent-libraries ${ASCEND_HOME_PATH}/tools/mssanitizer/lib64/libsanitizer_stub_dav-c220-cube.a
     )
 else()
-    set(SANITIZER_FLAGS)
+    set(SANITIZER_FLAGS) # 空
     set(SANITIZER_DEPEND_LIBS)
 endif()
 set(CCE_COMPILE_OPTION
-    -02 -std=gnu++17
+    -O2 -std=gnu++17
     --cce-aicore-only
     --cce-aicore-only
     -Wno-deprecated-declarations
     ${SANITIZER_FLAGS}
     "SHELL:-mllvm -cce-aicore-long-call"
-    "SHELL:-mllvm -cce-aicore-function-stach-size=16000"
+    "SHELL:-mllvm -cce-aicore-function-stack-size=16000"
     "SHELL:-mllvm -cce-aicore-record-overflow=false"
     "SHELL:-mllvm -cce-aicore-addr-transform"
     "SHELL:-mllvm -cce-aicore-addr-transform"
     "SHELL:-mllvm --cce-aicore-jump-expand=true"
 )
 set(PRIVATE_CCEC_PATH ${CMAKE_SOURCE_DIR}/3rdparty/compiler)
+# 设置包含路径
 if (EXISTS ${PRIVATE_CCEC_PATH})
-    message(STATUS "Using suctom ccec include directories")
+    message(STATUS "Using custom ccec include directories")
     set(CCE_INCLUDE_BASE ${PRIVATE_CCEC_PATH})
 else()
     set(CCE_INCLUDE_BASE ${ASCEND_HOME_PATH}/${ARCH}-linux)
@@ -46,6 +49,6 @@ include_directories(
     ${ASCEND_HOME_PATH}/toolkit/toolchain/hcc/aarch64-target-linux-gnu/include/c++/7.3.0
     ${ASCEND_HOME_PATH}/toolkit/toolchain/hcc/aarch64-target-linux-gnu/include/c++/7.3.0/aarch64-target-linux-gnu/
     ${CCE_INCLUDE_BASE}/tikcpp/tikcfw/
-    ${CCE_INCLUDE_BASE}/tikcpp/tikcfw/interfaces/
+    ${CCE_INCLUDE_BASE}/tikcpp/tikcfw/interface/
     ${CCE_INCLUDE_BASE}/tikcpp/tikcfw/impl/
 ) 
