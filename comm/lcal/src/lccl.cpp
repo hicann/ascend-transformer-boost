02e77587c157ced2960ed026ee2cc5096bea4165
He Changcheng <hcc2303@163.com>
2025-08-19 16:31:17 +0800

    draft

--- comm/lcal/src/lccl.cpp
+++ comm/lcal/src/lccl.cpp
@@ -23,3 +23,46 @@
 using namespace std;
 using namespace chrono;
 using namespace Mki;
+
+namespace Lcal {
+
+uint32_t GetLocalReduceBlockDum(int64_t dataSize)
+{
+    constexpr int oneDataSize = 190 * 1024;
+    constexpr int maxBlockDim = 8;
+    int blockDim = dataSize / oneDataSize + 1;
+    return blockDim <= maxBlockDim ? blockDim : maxBlockDim;
+}
+
+bool GetParallel()
+{
+    static int parallel = -1;
+    if (parallel == -1) {
+        static const char *ENV = Mki::GetEnv("LCCL_PARALLEL");
+        parallel = (ENV && (string(ENV) == "1" || string(ENV) == "true")) ? 1 : 0;
+        MKI_LOG(INFO) << "LCCL_PARALLEL is " << parallel;
+    }
+    return static_cast<bool>(parallel);
+}
+
+uint32_t GetAllReduceDetermBlockNum(uint32_t rankSize, int64_t dataSize, uint32_t extraFlag)
+{
+    constexpr uint32_t quickOneshotRankSize = 2;
+    constexpr uint32_t twoBlockNum = 2;
+    constexpr uint32_t treeBlockNum = 3;
+    constexpr uint32_t rankSize910a3 = 16;
+    constexpr uint32_t dbRingBlockNum = 34;
+    constexpr int64_t smallDataSize = 1 * 1024 * 1024;
+    constexpr int32_t smallDataSize910a3 = 32 * 1024 * 1024;
+    if ((extraFlag & ExtraFlag::TOPO_910_93) != 0) {
+        
+    }
+
+    uint32_t blockDim = GetLocalReduceBlockDum(dataSize);
+    return (rankSize + blockDim - 1) / blockDim;
+}
+
+
+
+
+}
