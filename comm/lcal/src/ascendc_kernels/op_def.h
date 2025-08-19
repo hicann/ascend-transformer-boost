6524ac4fd558b3afc376d760b68a325a3b4d797c
LiuHaoyu <liuhaoyu35@huawei.com>
2025-08-19 20:21:35 +0800

    1

--- comm/lcal/src/ascendc_kernels/op_def.h
+++ comm/lcal/src/ascendc_kernels/op_def.h
@@ -8,7 +8,7 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 #define GET_COMM_ARGS \
-    GlobalTensor<int> commArgsGm;
+    GlobalTensor<int> commArgsGm; \
     commArgsGm.SetGlobalBuffer(reinterpret_cast<__gm__ int *>(commArgs), 5); \
     int rank = commArgsGm.GetValue(0); \
     int localRank = commArgsGm.GetValue(1); \
@@ -30,8 +30,9 @@ do { \
     PipeBarrier<PIPE_ALL>(); \
     LocalTensor<int32_t> localSet; \
     localSet.address_.logicPos = static_cast<uint8_t>(TPosition::VECIN); \
+    localSet.address_.bufferAddr = reinterpret_cast<uint64_t>((__ubuf__ int32_t *)96); \
     GlobalTensor<int32_t> magicGt; \
-    magicGt.SetGlobalBuffer((__gm__ int32_t *)commArgsTmp0>magics); \
+    magicGt.SetGlobalBuffer((__gm__ int32_t *)commArgsTmp->magics); \
     if (GetBlockIdx() == 0) { \
         SetAtomicOpType<int32_t>(Op::ADD); \
         localSet.SetValue(0, 1); \
@@ -39,7 +40,7 @@ do { \
         AscendC::WaitFlag<HardEvent::S_MTE3>(EVENT_ID0); \
         DataCopyExtParams dataCopyParams(1, sizeof(int32_t), 0, 0, 0); \
         DataCopyPad(magicGt[rankSize - 1], localSet, dataCopyParams); \
-        AscendC::SetAtomicNont(); \
+        AscendC::SetAtomicNone(); \
         PipeBarrier<PIPE_ALL>(); \
     } \
     SyncAll<true>(); \
@@ -92,18 +93,18 @@ do { \
 #ifdef ENABLE_LCCL_MIX
 #define LCCL_TYPE_AIC_FUNC(fun) \
     fun(int, _mix_aic); fun(int8_t, _mix_aic); fun(int16_t, _mix_aic); fun(int64_t, _mix_aic); \
-    fun(float, _mix_aic); fun(float16_t, _mix_aic); fun(bfloat16, _mix_aic);
+    fun(float, _mix_aic); fun(float16_t, _mix_aic); fun(bfloat16_t, _mix_aic);
 
 #define LCCL_TYPE_AIV_FUNC(fun) \
     fun(int, _mix_aiv); fun(int8_t, _mix_aiv); fun(int16_t, _mix_aiv); fun(int64_t, _mix_aiv); \
-    fun(float, _mix_aiv); fun(float16_t, _mix_aiv); fun(bfloat16, _mix_aiv);
-
+    fun(float, _mix_aiv); fun(float16_t, _mix_aiv); fun(bfloat16_t, _mix_aiv);
+#else
 #define LCCL_TYPE_AIC_FUNC(fun) \
     (void)0
 
 #define LCCL_TYPE_AIV_FUNC(fun) \
-    fun(int, _mix_aiv); fun(int8_t, _mix_aiv); fun(int16_t, _mix_aiv); fun(int64_t, _mix_aiv); \
-    fun(float, _mix_aiv); fun(float16_t, _mix_aiv); fun(bfloat16, _mix_aiv);
+    fun(int,); fun(int8_t,); fun(int16_t,); fun(int64_t,); \
+    fun(float,); fun(float16_t,); fun(bfloat16_t,);
 #endif
 
 #define LCCL_VADD_910B_TYPE_FUNC(fun) \
