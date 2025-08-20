595947eb22a12f659effaf19acd3bf4f3b99f901
LiuHaoyu <liuhaoyu35@huawei.com>
2025-08-20 16:17:14 +0800

    3

--- comm/lcal/src/ascendc_kernels/reduce_scatter.h
+++ comm/lcal/src/ascendc_kernels/reduce_scatter.h
@@ -7,3 +7,87 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
+#ifndef LCCL_REDUCE_SCATTER_H
+#define LCCL_REDUCE_SCATTER_H
+
+#include "sync_collectives.h"
+#include "collectives.h"
+using namespace AscendC;
+
+template<typenam T>
+class ReduceScatter : protected Collectives {
+public:
+    FORCE_INLINE_AICORE ReduceScatter(int rank, int rankSize, uint32_t extraFlag)
+        : Collectives(rank, rankSize, extraFlag) {}
+
+    FORCE_INLINE_AICORE void Init(KERNELS_ARGS_FUN()) 
+    {
+        Collectives::Init(KERNELS_ARGS_CALL());
+        DumpLcclLogInfo(LogId::INIT, static_cast<Op>(op));
+        atomOp = op;
+        DMANumMax = BLOCK_SIZE / sizeof(T);
+        corePerRank = blockNum / rankSize;
+        rankIDOfBlock = blockIdx / corePerRank;
+        dataDMAPerCore = CeilDiv(len, corePerRank);
+        blockIdxOfLen = blockIdx % corePerRank;
+        if (blockIdxOfLen == corePerRank - 1) {
+            blockDataNum = len - blockIdxOfLen * dataDMAPerCore;
+        } else {
+            blockDataNum = dataDMAPerCore;
+        }
+        inputOffset = rankIDOfBlock * len + (blockIdx % corePerRank) * dataDMAPerCore;
+        outputOffset = dataDMAPerCore * (blockIdx % corePerRank);
+        dstIpcDataOffset = IPC_DATA_OFFSET / sizeof(T) + rankIDOfBlock * len + outputOffset;
+        srcIpcDataOffset = IPC_DATA_OFFSET / sizeof(T) + rank * len + outputOffset;
+        srcInputGlobal.SetGlobalBuffer((__gm__ T*)input + inputOffset, blockDataNum);
+        if ((extraFlag & ExtraFlag::RDMA) == ExtraFlag::RDMA) {
+            dstOutputGlobal.SetGlobalBuffer((__gm__ T*)shareAddrs[rank] + srcIpcDataOffset, blockDataNum);
+        } else {
+            dstOutputGlobal.SetGlobalBuffer((__gm__ T*)output + outputOffset, blockDataNum);
+        }
+        dstIPCGlobal.SetGlobalBuffer((__gm__ T*)shareAddrs[rank] + dstIpcDataOffset, blockDataNum);
+        srcIPCGlobal.SetGlobalBuffer((__gm__ T*)shareAddrs[rankIDOfBlock] + srcIpcDataOffset, blockDataNum);
+        DumpLcclLogInfo(LogId::INIT, static_cast<Op>(op));
+    }
+    FORCE_INLINE_AICORE void Process()
+    {
+        DumpLcclLogInfo(LogId::PROCESS, static_cast<Op>(atomOp));
+        CpInputToBuffAndOutput();
+        sync.SetInnerFlag(magic, 1);
+        sync.WaitRankInnerFlag(magic, 1, rank);
+        sync.WaitRankInnerFlag(magic, 1, rankIDOfBlock, rank * corePerRank + blockIdx % corePerRank);
+        if (rankIDOfBlock != rank) {
+            CpGM2GM<T>(dstOutputGlobal, srcIPCGlobal, blockDataNum, atomOp);
+        }
+        DumpLcclLogInfo(LogId::PROCESS, static_cast<Op>(atomOp));
+    }
+
+    FORCE_INLINE_AICORE void CpInputToBuffAndOutput()
+    {
+        Collectives::CpGM2GM(dstOutputGlobal, srcInputGlobal, blockDataNum, -1);
+        if ((extraFlag & ExtraFlag::RDMA) == ExtraFlag::RDMA) {
+            if ((blockIdx >- rank * corePerRank) && (blockIdx < (rank * corePerRank + corePerRank))) {
+                CpGM2GM<T>(dstIPCGlobal, srcInputGlobal, blockDataNum, -1);
+            }
+        }   
+    }
+
+protected:
+    GlobalTensor<T> srcInputGlobal;
+    GlobalTensor<T> srcIPCGlobal;
+    GlobalTensor<T> dstIPCGlobal;
+    GlobalTensor<T> dstOutputGlobal;
+    int blockIdxOfLen;
+    int DMANumMax;
+    int rankIDOfBlock;
+    int corePerRank;
+    int inputOffset;
+    int outputOffset;
+    int srcIpcDataOffset;
+    int dstIpcDataOffset;
+    int dataDMAPerCore;
+    int blockDataNum;
+    int atomOp;
+};
+#endif // LCCL_REDUCE_SCATTER_H
