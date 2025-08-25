55a2861f835b0096b8bdd30c6ed95c6b20164331
wanyukang <wanyukang@h-partners.com>
2025-08-25 10:18:58 +0800

    int

--- src/kernels/kernels/norm/layernormquant/tiling/layer_norm_quant_tiling.cpp
+++ src/kernels/kernels/norm/layernormquant/tiling/layer_norm_quant_tiling.cpp
@@ -117,7 +117,7 @@ Status LayerNormF16QuantTiling(const LaunchParam &launchParam, KernelInfo &kerne
                                 layerNormPtrCon.numCol;
         MKI_CHECK(layerNormPtrCon.numCol <= (UINT_MAX / FP16_OTHER_USED),
                   "sumData is invalid!", return Status::FailStatus(ERROR_INVALID_VALUE, "sumData is invalid!"));
-        uint32_t sumData = layerNormPtrCon.maxEleFp16 - NUM_TEMP_BUF -
+        int32_t sumData = layerNormPtrCon.maxEleFp16 - NUM_TEMP_BUF -
                            static_cast<uint32_t>(FP16_OTHER_USED) * layerNormPtrCon.numCol - SCALAR_USED;
         ret = CheckSplit(tilingDataPtr, totalMemNeed, sumData, layerNormPtrCon);
         OP_TILING_CHECK_STATUS_RETURN(ret);
