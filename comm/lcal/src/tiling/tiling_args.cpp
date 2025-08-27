bac2d644a57a955cb73cf53073fc18308c820bcd
He Changcheng <hcc2303@163.com>
2025-08-27 10:36:29 +0800

    draft

--- comm/lcal/src/tiling/tiling_args.cpp
+++ comm/lcal/src/tiling/tiling_args.cpp
@@ -11,28 +11,28 @@
 #include "tiling_args.h"
 
 namespace Lcal {
-    const char* CoCTilingData::ToString()
+    const char* CoCTilingData::ToString() const
     {
         std::string str =
-            "[CoCTilingData]: \n m=" + std::to_string(m) + ", k=" + std::to_string(k) + ", n=" + std::to_string(n) +
+            "[CoCTilingData]: \nm=" + std::to_string(m) + ", k=" + std::to_string(k) + ", n=" + std::to_string(n) +
             ", batchSize=" + std::to_string(batchSize) +
             ", \nblockDim=" + std::to_string(blockDim) + ", rank=" + std::to_string(rank) +
             ", rankSize=" + std::to_string(rankSize) + ", tag=" + std::to_string(tag) +
             ", \nmLoop=" + std::to_string(mLoop) + ", kLoop=" + std::to_string(kLoop) +
             ", nLoop=" + std::to_string(nLoop) + ", coreLoop=" + std::to_string(coreLoop) +
             ", tilingKey=" + std::to_string(tilingKey) +
-            ", m0="  + std::to_string(m0) + ", k0=" + std::to_string(k0) + ", n0=" + std::to_string(n0) +
+            ", \nm0="  + std::to_string(m0) + ", k0=" + std::to_string(k0) + ", n0=" + std::to_string(n0) +
             ", swizzlCount=" + std::to_string(swizzlCount) + ", swizzlDirect=" + std::to_string(swizzlDirect) +
             ", pValue=" + std::to_string(pValue) + ", ubMoveNum=" + std::to_string(ubMoveNum) +
             ", commNpuSplit=" + std::to_string(commNpuSplit) + ", commDataSplit=" + std::to_string(commDataSplit) +
             ", commDirect=" + std::to_string(commDirect) + ", lenPerLoop=" + std::to_string(lenPerLoop) +
-            ", extraUbMoveNum=" + std::to_string(extraUbMoveNum) +
+            ", \nextraUbMoveNum=" + std::to_string(extraUbMoveNum) +
             ", extraCommNpuSplit=" + std::to_string(extraCommNpuSplit) +
             ", extraCommDataSplit=" + std::to_string(extraCommDataSplit) +
             ", extraCommDirect=" + std::to_string(extraCommDirect) +
-            ", extraLenPerLoop=" + std::to_string(extraLenPerLoop) + ", splitK=" + std::to_string(splitK) +
+            ", extraLenPerLoop=" + std::to_string(extraLenPerLoop) + ", \nsplitK=" + std::to_string(splitK) +
             ", write2OtherRank=" + std::to_string(write2OtherRank) +
-            ", withSerialMode=" + std::to_string(withSerialMode) + ", is91093=" + std::to_string(is91093);
+            ", withSerialMode=" + std::to_string(withSerialMode) + ", \nis_91093=" + std::to_string(is91093);
         return str.data();
     }
 
@@ -41,7 +41,7 @@ namespace Lcal {
         m0 = m < n ? DEFAULT_COL : DEFAULT_ROW;
         k0 = DEFAULT_COL;
         n0 = m0 == DEFAULT_COL ? DEFAULT_ROW : DEFAULT_COL;
-        swizzlCount = DEFAULT_SWIZZL_COUNT;
+        swizzlCount = DEFAULT_SWIZZLE_COUNT;
         swizzlDirect = m > n ? SWIZZLE_DIRECT_ZERO : SWIZZLE_DIRECT_ONE;
         pValue = DEFAULT_P_VALUE;
         ubMoveNum = MAX_UB_NUM;
