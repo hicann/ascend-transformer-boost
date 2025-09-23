65a220b3bc4bb99914137bab4493364b94b06997
huangxiaolan <huangxiaolan7@huawei.com>
2025-09-23 16:20:09 +0800

    fix expression

--- src/kernels/mixkernels/fusion/tiling/fusion_tiling.cpp
+++ src/kernels/mixkernels/fusion/tiling/fusion_tiling.cpp
@@ -22,9 +22,9 @@ Status FusionTiling(const LaunchParam &launchParam, KernelInfo &kernelInfo)
     OpParam::Fusion fusionType = launchParam.GetParam<OpParam::Fusion>();
     std::string path(std::getenv("HOME"));
     path += std::string("/.atb_auto_fusion/bishengir_bin/") +
-            ((fusionType.fusionType ? "libmatmul_add.so" : "libmatmul_gelu.so") == OpParam::Fusion::MATMUL_ADD);
+            (fusionType.fusionType == OpParam::Fusion::MATMUL_ADD ? "libmatmul_add.so" : "libmatmul_gelu.so");
     std::string inferWorkspaceFuncName =
-        ((fusionType.fusionType ? "matmul_add_" : "matmul_gelu_") == OpParam::Fusion::MATMUL_ADD);
+        (fusionType.fusionType == OpParam::Fusion::MATMUL_ADD ? "matmul_add_" : "matmul_gelu_");
     FusionTilingData *tilingDataPtr = reinterpret_cast<FusionTilingData *>(kernelInfo.GetTilingHostAddr());
     void *handle = dlopen(path.c_str(), RTLD_LAZY);
     if (!handle) {
