8dfdb58f34c4f243325bf518047484aad5869a60
huangxiaolan <huangxiaolan7@huawei.com>
2025-09-23 09:36:42 +0800

    fix include

--- src/ops_infer/multi_latent_attention/atb_acl_mla.cpp
+++ src/ops_infer/multi_latent_attention/atb_acl_mla.cpp
@@ -8,7 +8,7 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 #include "atb/atb_acl.h"
-#include "atb_acl_util.h"
+#include "atb/utils/atb_acl_util.h"
 #include "atb/operation/operation_base.h"
 
 #ifdef __cplusplus
