c570a872bad5928197dc871728312ed5456c4d2a
huangxiaolan <huangxiaolan7@huawei.com>
2025-09-24 09:35:01 +0800

    fix include

--- src/kernels/tbe_adapter/platform/tiling/platform/platform_ascendc.h
+++ src/kernels/tbe_adapter/platform/tiling/platform/platform_ascendc.h
@@ -18,7 +18,7 @@
 
 #include <cstdint>
 #include <mutex>
-#include "stubs\include\metadef\inc\external\platform\platform_info.h"
+#include "stubs/include/metadef/inc/external/platform/platform_info.h"
 
 #define ASCENDC_ASSERT(cond, behavior) \
     do {                               \
