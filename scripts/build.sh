3fde731673c17bf1519d17530a97c8f35491e5ce
AtlasAccount <AtlasAccount@noreply.gitcode.com>
2025-09-27 11:53:28 +0800

    ci测试勿合
    Signed-off-by: AtlasAccount <AtlasAccount@noreply.gitcode.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (c) 2024 Huawei Technologies Co., Ltd.
-# This file is a part of the CANN Open Software.
+# This file is a part of the CANN Open Software.ci
 # Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
