6d50e1bc08cf0751269a4a8315edb2ca836091f6
qq_44359711 <caobingjie@huawei.com>
2025-10-09 14:15:26 +0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -158,7 +158,7 @@ function fn_build_mki()
         branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || echo "commit_id")
         [[ "$branch" == *br_personal* || "$branch" == "commit_id" || "$branch" == *revert-mr* ]] && branch=master
         echo  "current branch for mki: $branch"
-        git clone --branch $branch --depth 1 https://gitcode.com/cann/ascend-transformer-boost.git Mind-KernelInfra
+        git clone --branch $branch --depth 1 https://gitcode.com/cann/ascend-boost-comm.git Mind-KernelInfra
     fi
     cd Mind-KernelInfra
     echo  "current commid id of Mind-KernelInfra: $(git rev-parse HEAD)"
