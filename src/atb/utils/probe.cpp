24a1c3b52e96237b212989199f664c6e3b37f684
chenwei <chenwei542@huawei.com>
2025-10-10 17:31:29 +0800

    fix

--- src/atb/utils/probe.cpp
+++ src/atb/utils/probe.cpp
@@ -178,7 +178,7 @@ void Probe::ReportOverflowKernel(const std::string &kernelPath)
 
 void Probe::UpdateConfig() {}
 
-bool IsSaveTensorInSpecificDir(const std::string &tensorDir)
+bool Probe::IsSaveTensorInSpecificDir(const std::string &tensorDir)
 {
     (void)tensorDir;
     return true;
