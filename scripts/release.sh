35b3d3745cd10dfbbdb8751372d74e81cd3d5823
qq_44359711 <caobingjie@huawei.com>
2025-10-11 17:31:52 +0800

    update: 更新文件 release.sh
    Signed-off-by: qq_44359711 <caobingjie@huawei.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,7 +16,7 @@ function fn_build_mki()
     if [ ! -d "$THIRD_PARTY_DIR"/Mind-KernelInfra ]; then
         [[ ! -d "$THIRD_PARTY_DIR" ]] && mkdir $THIRD_PARTY_DIR
         cd $THIRD_PARTY_DIR
-        branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || echo "commit_id")
+        branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || echo "commit_id") 
         [[ "$branch" == *br_personal* || "$branch" == "commit_id" ]] && branch=master
         echo  "current branch for atb and mki: $branch"
         git clone --branch $branch --depth 1 https://gitcode.com/cann/ascend-boost-comm.git Mind-KernelInfra
