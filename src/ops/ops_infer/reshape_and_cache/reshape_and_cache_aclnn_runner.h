ebc6a2a8201e7a7419860a32032b7c803d412677
sakya_gyq <geyunqi@huawei.com>
2026-03-03 10:46:46 +0800

    950 reshape and cache
    Co-authored-by: sakya_gyq<geyunqi@huawei.com>

--- src/ops/ops_infer/reshape_and_cache/reshape_and_cache_aclnn_runner.h
+++ src/ops/ops_infer/reshape_and_cache/reshape_and_cache_aclnn_runner.h
@@ -7,52 +7,56 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
-
-#ifndef ATB_RMS_NORM_ACLNN_RUNNER_H
-#define ATB_RMS_NORM_ACLNN_RUNNER_H
+#ifndef ATB_RESHAPE_AND_CACHE_ACLNN_RUNNER_H
+#define ATB_RESHAPE_AND_CACHE_ACLNN_RUNNER_H
 #include "atb/infer_op_params.h"
 #include "atb/runner/aclnn_runner.h"
 
-using AclnnGetWorkspaceSizeFunc = aclnnStatus (*)(
-    const aclTensor *,   // key
-    const aclTensor *,   // keyCacheRef
-    const aclTensor *,   // slotMapping
-    const aclTensor *,   // value
-    const aclTensor *,   // valueCacheRef
-    const aclTensor *,   // compressLensOptional
-    const aclTensor *,   // compressSeqOffsetOptional
-    const aclTensor *,   // seqLensOptional
-    char *,              // cacheModeOptional
-    char *,              // scatterModeOptional
-    const aclIntArray *, // stridesOptional
-    const aclIntArray *, // offsetsOptional
-    uint64_t *,          // workspaceSize
-    aclOpExecutor **     // executor
-);
-
-using AclnnExecuteFunc = aclnnStatus (*)(
-    void *,//workspace
-    uint64_t,//workspaceSize
-    aclOpExecutor *,//executor
-    aclrtStream//stream
-);
+using AclnnScatterPaKvCacheGetWorkspaceSizeFunc = aclnnStatus (*)(const aclTensor *key, const aclTensor *keyCacheRef,
+    const aclTensor *slotMapping, const aclTensor *value, const aclTensor *valueCacheRef,
+    const aclTensor *compressLensOptional, const aclTensor *compressSeqOffsetOptional, const aclTensor *seqLensOptional,
+    char *cacheModeOptional, char *scatterModeOptional, const aclIntArray *stridesOptional,
+    const aclIntArray *offsetsOptional, uint64_t *workspaceSize, aclOpExecutor **executor);
+using AclnnScatterPaKvCacheFunc = aclnnStatus (*)(
+    void *workspace, uint64_t workspaceSize, aclOpExecutor *executor, aclrtStream stream);
 
 namespace atb {
 class ReshapeAndCacheAclnnRunner : public AclnnRunner {
 public:
     explicit ReshapeAndCacheAclnnRunner(const infer::ReshapeAndCacheParam &param);
     ~ReshapeAndCacheAclnnRunner() override;
-    static Status LoadMethod();
+    static Status LoadAclnnFuncs();
 
 protected:
     Status BuildAclnnVariantPack(const RunnerVariantPack &runnerVariantPack) override;
-    Status LaunchAclnnKernel() override;
     aclnnStatus SetAclNNWorkspaceExecutor() override;
+    Status LaunchAclnnKernel() override;
+
+private:
+    void GetTensorNum();
+    void InitTensorIndex();
+    Status CreateKeyAclnnTensor();
+    Status CreateValueAclnnTensor();
+    Status CreateKeyCacheAclnnTensor();
+    Status CreateValueCacheAclnnTensor();
+    Status CreateSlotMappingAclnnTensor();
 
 private:
     infer::ReshapeAndCacheParam param_;
-    static AclnnGetWorkspaceSizeFunc aclnnGetWorkspaceSizeFunc_;
-    static AclnnExecuteFunc aclnnExecuteFunc_;
+
+    size_t aclInTensorNum_ = 0;
+
+    size_t atbInTensorIndex_ = 0;
+    size_t aclInTensorIndex_ = 0;
+
+    size_t keyAclTensorIndex_ = 0;
+    size_t valueAclTensorIndex_ = 0;
+    size_t keyCacheRefAclTensorIndex_ = 0;
+    size_t valueCacheRefAclTensorIndex_ = 0;
+    size_t slotMappingAclTensorIndex_ = 0;
+
+    static AclnnScatterPaKvCacheGetWorkspaceSizeFunc aclnnScatterPaKvCacheGetWorkspaceSizeFunc_;
+    static AclnnScatterPaKvCacheFunc aclnnScatterPaKvCacheFunc_;
 };
-} // namespace atb
+}  // namespace atb
 #endif
