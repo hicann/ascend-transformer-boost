aa61e04ec59cb80d28c96394f9f5a2e328020af9
sakya_gyq <geyunqi@huawei.com>
2026-03-03 16:13:22 +0800

    fix rmsnorm rstd shape
    Co-authored-by: sakya_gyq<geyunqi@huawei.com>

--- src/ops/ops_infer/rms_norm/rms_norm_aclnn_runner.h
+++ src/ops/ops_infer/rms_norm/rms_norm_aclnn_runner.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -13,22 +13,11 @@
 #include "atb/infer_op_params.h"
 #include "atb/runner/aclnn_runner.h"
 
-using AclnnGetWorkspaceSizeFunc = aclnnStatus (*)(
-    const aclTensor *,// x
-    const aclTensor *,// gamma
-    double,//epsilon
-    const aclTensor *,// yOut
-    const aclTensor *,//rstdOut
-    uint64_t *,//workspaceSize
-    aclOpExecutor ** //executor
-);
+using AclnnRmsNormGetWorkspaceSizeFunc = aclnnStatus (*)(const aclTensor *x, const aclTensor *gamma, double epsilon,
+    const aclTensor *yOut, const aclTensor *rstdOut, uint64_t *workspaceSize, aclOpExecutor **executor);
 
-using AclnnExecuteFunc = aclnnStatus (*)(
-    void *,//workspace
-    uint64_t,//workspaceSize
-    aclOpExecutor *,//executor
-    aclrtStream//stream
-);
+using AclnnRmsNormFunc = aclnnStatus (*)(
+    void *workspace, uint64_t workspaceSize, aclOpExecutor *executor, aclrtStream stream);
 
 namespace atb {
 class RmsNormAclnnRunner : public AclnnRunner {
@@ -36,31 +25,39 @@ public:
     explicit RmsNormAclnnRunner(const infer::RmsNormParam &param);
     ~RmsNormAclnnRunner() override;
 
-    static Status LoadMethod();
+    static Status LoadAclnnFuncs();
+
 protected:
     Status BuildAclnnVariantPack(const RunnerVariantPack &runnerVariantPack) override;
-    Status LaunchAclnnKernel() override;
     aclnnStatus SetAclNNWorkspaceExecutor() override;
-    aclnnStatus  CreateAclTensor(const std::vector<int64_t>& shape, void** deviceAddr, aclDataType dataType,
-                                 aclTensor** tensor, uint64_t dataSize);
+    Status LaunchAclnnKernel() override;
 
 private:
-    Status CreateInputAclnnTensor();
+    void GetTensorNum();
+    void InitTensorIndex();
+    Status CreateXAclnnTensor();
     Status CreateGammaAclnnTensor();
-    Status CreateOutputAclnnTensor();
-    Status CreateRstdAclnnTensor();
+    Status CreateYOutAclnnTensor();
+    Status CreateRstdOutAclnnTensor();
 
 private:
     infer::RmsNormParam param_;
-    //两个函数指针分别对应对应aclnnop/aclnn_rms_norm.h中的两段式接口
-    //GetWorkSpace接口
-    static AclnnGetWorkspaceSizeFunc aclnnGetWorkspaceSizeFunc_;
-    //调用接口
-    static AclnnExecuteFunc aclnnExecuteFunc_;
 
-    void* rstdDeviceAddr_ = nullptr;
-    aclTensor* rstdTensor_ = nullptr;
+    size_t aclInTensorNum_ = 0;
+    size_t aclOutTensorNum_ = 0;
 
+    size_t atbInTensorIndex_ = 0;
+    size_t aclInTensorIndex_ = 0;
+    size_t atbOutTensorIndex_ = 0;
+    size_t aclOutTensorIndex_ = 0;
+
+    size_t xAclTensorIndex_ = 0;
+    size_t gammaAclTensorIndex_ = 0;
+    size_t yOutAclTensorIndex_ = 0;
+    size_t rstdOutAclTensorIndex_ = 0;
+
+    static AclnnRmsNormGetWorkspaceSizeFunc aclnnRmsNormGetWorkspaceSizeFunc_;
+    static AclnnRmsNormFunc aclnnRmsNormFunc_;
 };
-} // namespace atb
+}  // namespace atb
 #endif
