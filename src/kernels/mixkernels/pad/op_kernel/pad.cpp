75c40e8dd24b3c70d9c0abf7212487c46ce37338
ninihihi <zouyanlong@h-partners.com>
2026-04-02 10:40:29 +0800

    fix
    Co-authored-by: zouyanlong<zouyanlong@h-partners.com>

--- src/kernels/mixkernels/pad/op_kernel/pad.cpp
+++ src/kernels/mixkernels/pad/op_kernel/pad.cpp
@@ -113,7 +113,7 @@ private:
 
 inline __aicore__ void InitTilingData(const __gm__ uint8_t *p_tilingdata, AtbOps::PadTilingData *tilingdata)
 {
-#if defined(__CCE_KT_TEST__) || (__CCE_AICORE__ == 220)
+#if defined(__CCE_KT_TEST__) || (defined(__CCE_AICORE__) && __CCE_AICORE__ == 220)
     tilingdata->padLength = (*(const __gm__ uint32_t *)(p_tilingdata + 0));
     tilingdata->batch = (*(const __gm__ uint32_t *)(p_tilingdata + 4));
     tilingdata->hiddenDim = (*(const __gm__ uint32_t *)(p_tilingdata + 8));
